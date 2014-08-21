9cf4754bcde04f39ec45eefbf5f75fd4bb23fdf7
Adina Tusa <adina.tusa@hp.com>
2014-08-21 10:00:57 +0300

    HTTP Client - JUnits added.

--- score-http-client/src/test/java/org/score/content/httpclient/build/SSLConnectionSocketFactoryBuilderTest.java
+++ score-http-client/src/test/java/org/score/content/httpclient/build/SSLConnectionSocketFactoryBuilderTest.java
@@ -11,6 +11,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.net.ssl.SSLContext;
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.security.KeyStore;
 
@@ -20,6 +21,7 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
@@ -28,14 +30,13 @@ import static org.powermock.api.mockito.PowerMockito.whenNew;
  * Date: 8/20/14
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({SSLContexts.class, System.class, SSLConnectionSocketFactoryBuilder.class})
+@PrepareForTest({SSLContexts.class, System.class, SSLConnectionSocketFactoryBuilder.class, KeyStore.class})
 public class SSLConnectionSocketFactoryBuilderTest {
 
     public static final String BAD_TRUST_KEYSTORE_ERROR = "The trust keystore provided in the 'trustKeystore' input is corrupted OR the password (in the 'trustPassword' input) is incorrect";
     public static final String BAD_KEYSTORE_ERROR = "The keystore provided in the 'keystore' input is corrupted OR the password (in the 'keystorePassword' input) is incorrect";
     public static final String PASSWORD = "password";
     public static final String KEYSTORE = "C:/keystore";
-
     @Rule
     public ExpectedException exception = ExpectedException.none();
     @Mock
@@ -51,6 +52,8 @@ public class SSLConnectionSocketFactoryBuilderTest {
     private SSLConnectionSocketFactory sslsfMock;
     @Mock
     private KeyStore keyStoreMock;
+    @Mock
+    private InputStream inputStreamMock;
 
     @Test
     public void build() throws Exception {
@@ -163,6 +166,29 @@ public class SSLConnectionSocketFactoryBuilderTest {
         exception.expectMessage(BAD_KEYSTORE_ERROR);
 
         builder.createKeystore(sslContextBuilderMock, true);
+    }
 
+    @Test
+    public void createKeyStoreWithException() throws Exception {
+        builder = new SSLConnectionSocketFactoryBuilder();
+
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Keystore url may not be null");
+
+        builder.createKeyStore(null, null);
+    }
+
+    @Test
+    public void createKeyStore() throws Exception {
+        builder = new SSLConnectionSocketFactoryBuilder();
+        mockStatic(KeyStore.class);
+        when(KeyStore.getInstance("jks")).thenReturn(keyStoreMock);
+        when(urlMock.openStream()).thenReturn(inputStreamMock);
+        doNothing().when(keyStoreMock).load(inputStreamMock, PASSWORD.toCharArray());
+        doNothing().when(inputStreamMock).close();
+
+        KeyStore keystore = builder.createKeyStore(urlMock, PASSWORD);
+        verify(keyStoreMock).load(inputStreamMock, PASSWORD.toCharArray());
+        assertEquals(keystore, keyStoreMock);
     }
 }
