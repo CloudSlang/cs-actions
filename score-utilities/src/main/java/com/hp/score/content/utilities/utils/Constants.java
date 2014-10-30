15356b51d5db83565423213567d62df42e313403
Alex-Persa <dan-alexandru.persa@hp.com>
2014-10-30 16:50:32 +0200

    Added Sleep and StringEquals Operations.

---
+++ score-utilities/src/main/java/com/hp/score/content/utilities/utils/Constants.java
@@ -0,0 +1,22 @@
+package com.hp.score.content.utilities.utils;
+
+/**
+ * Created by persdana on 10/30/2014.
+ */
+public class Constants {
+    public static final String SUCCESS = "success";
+    public static final String FAILURE = "failure";
+
+    public static final class OutputNames {
+        public static final String RETURN_RESULT = "returnResult";
+        public static final String RESPONSE_TEXT = "response";
+
+        public static final String RESULT_STRING = "ResultString";
+        public static final String RESULT_TEXT = "result";
+    }
+
+    public static final class ResponseNames {
+        public static final String SUCCESS = "success";
+        public static final String FAILURE = "failure";
+    }
+}
