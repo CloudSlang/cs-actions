1f68d0fa8c90ec9a3d057cc26e0b4634d2badf88
ioanvranauhp <ioan.vranau@hp.com>
2014-11-04 16:57:09 +0200

    renamed linearUpdateTask to LinearUpdateTask

---
+++ score-ssh/src/main/java/com/hp/score/content/ssh/utils/simulator/visualization/LinearUpdateTask.java
@@ -0,0 +1,46 @@
+package com.hp.score.content.ssh.utils.simulator.visualization;
+
+import com.hp.score.content.ssh.utils.simulator.visualization.ByteWrapper;
+import com.hp.score.content.ssh.utils.simulator.visualization.ScreenTask;
+
+public abstract class LinearUpdateTask extends ScreenTask {
+    int maxOffset;
+    int offsetIndex;
+    boolean completed;
+    boolean nextLine;
+    int newlines;
+
+    public LinearUpdateTask(int length) {
+        this.maxOffset = length;
+        completed = false;
+        nextLine = false;
+        newlines = 0;
+        offsetIndex = 0;
+    }
+
+    public abstract byte process(int offset);
+
+    public final void process(int x, int y, ByteWrapper val) {
+        nextLine = false;
+        byte read = process(offsetIndex);
+
+        if (read == (byte) ('\r' & 0xFF) || (read == (byte) ('\n' & 0xFF)
+                && process(offsetIndex - 1) != (byte) ('\r' & 0xFF))) {
+            nextLine = true;
+        } else {
+            val.setValue(read);
+        }
+        offsetIndex++;
+        if (offsetIndex >= maxOffset)
+            completed = true;
+    }
+
+    public boolean completed() {
+        return completed;
+    }
+
+    public boolean nextLine() {
+        return nextLine || completed;
+    }
+
+}
