691709533a8746833c921787609c1a9b8a03bddb
ioanvranauhp <ioan.vranau@hp.com>
2015-01-09 16:43:08 +0200

    added javadocs for actions. Also added java doc for http client package

--- score-mail/src/main/java/org/openscore/content/mail/actions/SendMailAction.java
+++ score-mail/src/main/java/org/openscore/content/mail/actions/SendMailAction.java
@@ -19,6 +19,37 @@ import java.util.Map;
  */
 public class SendMailAction {
 
+    /**
+     * The operation sends a smtp email.
+     *
+     * @param hostname The hostname or ip address of the smtp server.
+     * @param port The port of the smtp service.
+     * @param htmlEmail The value should be true if the email is in rich text/html format.
+     *                  The value should be false if the email is in plain text format.
+     *                  Valid values: true, false. Default value: true.
+     * @param from From email address.
+     * @param to A delimiter separated list of email address(es) or recipients where the email will be sent.
+     * @param cc A delimiter separated list of email address(es) or recipients, to be placed in the CC.
+     * @param bcc A delimiter separated list of email address(es) or recipients, to be placed in the BCC.
+     * @param subject The email subject. If a subject spans on multiple lines, it is formatted to a single one.
+     * @param body The body of the email.
+     * @param readReceipt The value should be true if read receipt is required, else false. Valid values: true, false. Default value: false.
+     * @param attachments A delimited separated list of files to attach (must be full path).
+     * @param user If SMTP authentication is needed, the username to use.
+     * @param password If SMTP authentication is needed, the password to use.
+     * @param delimiter A delimiter to separate the email recipients and the attachments. Default value: ','.
+     * @param characterSet The character set encoding for the entire email which includes subject, body,
+     *                     attached file name and the attached file.
+     *                     <br>Valid values: UTF-8, UTF-16, UTF-32, EUC-JP, ISO-2022-JP, Shift_JIS, Windows-31J. Default value: UTF-8.
+     * @param contentTransferEncoding The content transfer encoding scheme (such as 7bit, 8bit, base64, quoted-printable etc)
+     *                                for the entire email which includes subject, body, attached file name and the attached file. .
+     *                                Valid values: quoted-printable, base64, 7bit, 8bit, binary, x-token. Default value: quoted-printable (or Q Encoding).
+     * @return a map containing the output of the operation. The keys present in the map are
+     *      <br><b>returnResult</b> - that will contain the SentMailSuccessfully if the mail was sent successfully.
+     *      <br><b>returnCode</b> - the return code of the operation. 0 if the operation goes to success, -1 if the operation goes to failure.
+     *      <br><b>exception</b> - the exception message if the operation goes to failure.
+     * @throws Exception
+     */
     @Action(name = "Send Mail",
             outputs = {
                     @Output(SendMail.RETURN_RESULT),
