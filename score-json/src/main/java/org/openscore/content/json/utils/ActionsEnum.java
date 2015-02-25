0496748805a0bf776f825d850541a12c7c9cb990
ioanvranauhp <ioan.vranau@hp.com>
2015-02-25 14:41:29 +0200

    EditJson operation commited other files Signed-off-by: Ioan Vranau ioan.vranau@hp.com

--- score-json/src/main/java/org/openscore/content/json/utils/ActionsEnum.java
+++ score-json/src/main/java/org/openscore/content/json/utils/ActionsEnum.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *******************************************************************************/
 package org.openscore.content.json.utils;
 
 /**
@@ -5,18 +14,24 @@ package org.openscore.content.json.utils;
  * Date 2/17/2015.
  */
 public enum ActionsEnum {
-    get(Constants.EditJsonOperations.GET_ACTION),
-    insert(Constants.EditJsonOperations.INSERT_ACTION),
-    add(Constants.EditJsonOperations.ADD_ACTION),
-    update(Constants.EditJsonOperations.UPDATE_ACTION),
-    delete(Constants.EditJsonOperations.DELETE_ACTION);
+    get(Constants.EditJsonOperations.GET_ACTION, false),
+    insert(Constants.EditJsonOperations.INSERT_ACTION, true),
+    add(Constants.EditJsonOperations.ADD_ACTION, true),
+    update(Constants.EditJsonOperations.UPDATE_ACTION, true),
+    delete(Constants.EditJsonOperations.DELETE_ACTION, false);
+    private final Boolean needValue;
     private String value;
 
-    private ActionsEnum(String value) {
+    private ActionsEnum(String value, Boolean needValue) {
         this.value = value;
+        this.needValue = needValue;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public Boolean getNeedValue() {
+        return needValue;
     }
 }
