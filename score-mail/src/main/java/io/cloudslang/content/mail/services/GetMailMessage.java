ebacb41b16e36d060ccb54e90381591b61a9b09c
Alex-Persa <dan-alexandru.persa@hp.com>
2015-07-21 17:36:45 +0300

    Added timeout input to email operations.

--- score-mail/src/main/java/io/cloudslang/content/mail/services/GetMailMessage.java
+++ score-mail/src/main/java/io/cloudslang/content/mail/services/GetMailMessage.java
@@ -202,7 +202,7 @@ public class GetMailMessage {
     protected Store createMessageStore() throws Exception {
         Properties props = new Properties();
         if(timeout > 0) {
-            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.timeout",timeout);
         }
         Authenticator auth = new SimpleAuthenticator(username, password);
         Store store;
