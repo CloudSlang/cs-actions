d33b843f900a485eec4d78e933addcfdc02ccdfc
SamMarkowitz <sam.markowitz@hpe.com>
2016-03-03 15:25:17 +0200

    start refactor
    Signed-off-by: SamMarkowitz <sam.markowitz@hpe.com>

---
+++ score-xml/src/main/java/io/cloudslang/content/xml/entities/inputs/CustomInputs.java
@@ -0,0 +1,7 @@
+package io.cloudslang.content.xml.entities.inputs;
+
+/**
+ * Created by markowis on 03/03/2016.
+ */
+public class CustomInputs {
+}
---
+++ score-xml/src/main/java/io/cloudslang/content/xml/services/AddAttributeService.java
@@ -0,0 +1,7 @@
+package io.cloudslang.content.xml.services;
+
+/**
+ * Created by markowis on 03/03/2016.
+ */
+public class AddAttributeService {
+}
