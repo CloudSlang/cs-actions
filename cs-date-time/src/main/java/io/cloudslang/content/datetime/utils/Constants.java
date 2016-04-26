53fad9a60843e108d50f5202e7a06c9954f3e6cc
Curelea Stefan <stefan.curelea@enea.com>
2016-04-26 11:50:01 +0300

    Added the GetCurentDateTime @action.
    Signed-off-by: Curelea Stefan <stefan.curelea@enea.com>

---
+++ cs-date-time/src/main/java/io/cloudslang/content/datetime/utils/Constants.java
@@ -0,0 +1,29 @@
+package io.cloudslang.content.datetime.utils;
+
+/**
+ * Created by stcu on 22.04.2016.
+ */
+public class Constants {
+
+    public class OutputNames {
+        public static final String RETURN_RESULT = "returnResult";
+        public static final String EXCEPTION = "exception";
+        public static final String RETURN_CODE = "returnCode";
+        public static final String ERROR_MESSAGE = "errorMessage";
+    }
+
+    public class ReturnCodes {
+        public static final String RETURN_CODE_FAILURE = "-1";
+        public static final String RETURN_CODE_SUCCESS = "0";
+    }
+
+    public class ResponseNames {
+        public static final String SUCCESS = "success";
+        public static final String FAILURE = "failure";
+    }
+
+    public class InputNames {
+        public static final String LOCALE_LANG = "localeLang";
+        public static final String LOCALE_COUNTRY = "localeCountry";
+    }
+}
---
+++ cs-date-time/src/test/java/io/cloudslang/content/datetime/actions/GetCurrentDateTimeTest.java
@@ -0,0 +1,67 @@
+package io.cloudslang.content.datetime.actions;
+
+import io.cloudslang.content.datetime.utils.Constants;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by stcu on 25.04.2016.
+ */
+public class GetCurrentDateTimeTest {
+    private final GetCurrentDateTime getCurrentDateTime = new GetCurrentDateTime();
+
+    @Test
+    public void testExecuteAllValid() {
+        String localeLang = "da";
+        String localeCountry = "DK";
+
+        final Map<String, String> result = getCurrentDateTime.execute(localeLang, localeCountry);
+        assertTrue(!result.get(Constants.OutputNames.RETURN_RESULT).isEmpty());
+        assertEquals("0", result.get(Constants.OutputNames.RETURN_CODE));
+    }
+
+    @Test
+    public void testLocaleLangNull() {
+        String localeLang = null;
+        String localeCountry = "DK";
+
+        final Map<String, String> result = getCurrentDateTime.execute(localeLang, localeCountry);
+        assertTrue(!result.get(Constants.OutputNames.RETURN_RESULT).isEmpty());
+        assertEquals("0", result.get(Constants.OutputNames.RETURN_CODE));
+    }
+
+    @Test
+    public void testLocaleCountryNull() {
+        String localeLang = "da";
+        String localeCountry = null;
+
+        final Map<String, String> result = getCurrentDateTime.execute(localeLang, localeCountry);
+        assertTrue(!result.get(Constants.OutputNames.RETURN_RESULT).isEmpty());
+        assertEquals("0", result.get(Constants.OutputNames.RETURN_CODE));
+    }
+
+    @Test
+    public void testExecuteAllInvalid() {
+        String localeLang = "asdassssssasd";
+        String localeCountry = "asdasdasdasdasdasd";
+
+        final Map<String, String> result = getCurrentDateTime.execute(localeLang, localeCountry);
+        assertTrue(!result.get(Constants.OutputNames.RETURN_RESULT).isEmpty());
+        assertEquals("0", result.get(Constants.OutputNames.RETURN_CODE));
+    }
+
+    @Test
+    public void testExecuteTimestamp() {
+        String localeLang = "unix";
+        String localeCountry = "DK";
+
+        final Map<String, String> result = getCurrentDateTime.execute(localeLang, localeCountry);
+        assertTrue(!result.get(Constants.OutputNames.RETURN_RESULT).isEmpty());
+        assertTrue(result.get(Constants.OutputNames.RETURN_RESULT).startsWith("1461"));
+        assertEquals("0", result.get(Constants.OutputNames.RETURN_CODE));
+    }
+}
