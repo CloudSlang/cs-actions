eeaf446ba56b3900cdc00d672714fd8bddc15af5
Mihai Tusa <mihail-cristian.tusa@hp.com>
2016-05-24 16:28:01 +0300

    Start/Stop Instance on Amazon AWS EC2 - bug fixing on Start/Stop Server (Instance). Signed-off-by: Mihail-Cristian Tusa <mihail-cristian.tusa@hpe.com>

--- score-jClouds/src/main/java/io/cloudslang/content/jclouds/entities/constants/Constants.java
+++ score-jClouds/src/main/java/io/cloudslang/content/jclouds/entities/constants/Constants.java
@@ -18,8 +18,6 @@ public class Constants {
     public static final class Messages {
         public static final String HARD_REBOOT_SUCCESS = "Hard reboot started successfully.";
         public static final String SOFT_REBOOT_SUCCESS = "Soft reboot started successfully.";
-        public static final String SERVER_STARTED = "Server started.";
-        public static final String SERVER_STOPPED = "Server stopped.";
         public static final String SERVER_SUSPENDED = "Server suspended.";
         public static final String SERVER_RESUMED = "Server resumed.";
         public static final String SERVER_UPDATED = "Server updated successfully.";
--- score-jClouds/src/main/java/io/cloudslang/content/jclouds/execute/instances/StartServerExecutor.java
+++ score-jClouds/src/main/java/io/cloudslang/content/jclouds/execute/instances/StartServerExecutor.java
@@ -1,12 +1,10 @@
 package io.cloudslang.content.jclouds.execute.instances;
 
-import io.cloudslang.content.jclouds.entities.constants.Constants;
 import io.cloudslang.content.jclouds.entities.inputs.CommonInputs;
 import io.cloudslang.content.jclouds.entities.inputs.CustomInputs;
 import io.cloudslang.content.jclouds.factory.ComputeFactory;
 import io.cloudslang.content.jclouds.services.ComputeService;
 import io.cloudslang.content.jclouds.utils.OutputsUtil;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 
@@ -18,7 +16,6 @@ public class StartServerExecutor {
         ComputeService cs = ComputeFactory.getComputeService(inputs);
         String resultStr = cs.start(customInputs.getRegion(), customInputs.getServerId());
 
-        return OutputsUtil.getResultsMap((StringUtils.isBlank(resultStr)) ?
-                Constants.Messages.SERVER_STARTED : Constants.Miscellaneous.EMPTY);
+        return OutputsUtil.getResultsMap(resultStr);
     }
 }
