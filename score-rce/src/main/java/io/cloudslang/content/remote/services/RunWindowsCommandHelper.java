a10b6f1febfbb878f55f9b7c9f66e70c985517c9
Daria Pasternak <daria.pasternak@hpe.com>
2016-06-20 09:32:18 +0300

    fixed CommandUtils and inputs

--- score-rce/src/main/java/io/cloudslang/content/remote/services/RunWindowsCommandHelper.java
+++ score-rce/src/main/java/io/cloudslang/content/remote/services/RunWindowsCommandHelper.java
@@ -1,10 +1,5 @@
 package io.cloudslang.content.remote.services;
 
-import io.cloudslang.content.remote.constants.Constants;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 
 /**
