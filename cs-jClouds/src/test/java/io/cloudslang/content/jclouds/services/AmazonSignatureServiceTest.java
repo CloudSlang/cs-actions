de7183a8c7966d7fe52fefc48c1b2f2eaa51fa22
Mihai Tusa <mihail-cristian.tusa@hp.com>
2016-08-12 15:54:55 +0300

    Amazon-AWS-EC2-SignatureV4 - corrected failing unit test 3. Signed-off-by: Mihail-Cristian Tusa <mihail-cristian.tusa@hpe.com>

--- cs-jClouds/src/test/java/io/cloudslang/content/jclouds/services/AmazonSignatureServiceTest.java
+++ cs-jClouds/src/test/java/io/cloudslang/content/jclouds/services/AmazonSignatureServiceTest.java
@@ -9,6 +9,7 @@ import java.security.SignatureException;
 import java.util.Map;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by Mihai Tusa.
@@ -18,11 +19,6 @@ public class AmazonSignatureServiceTest {
     @Test
     public void testSignature() throws SignatureException, MalformedURLException {
         String expectedSignature = "f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41";
-        String expectedAuthorizationHeader = "x-amz-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\n" +
-                "Authorization:AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request, SignedHeaders=host;range;x-amz-content-sha256;x-amz-date, Signature=f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41\n" +
-                "x-amz-date:20130524T000000Z\n" +
-                "host:examplebucket.s3.amazonaws.com\n" +
-                "range:bytes=0-9\n";
 
         String headers = "Range:bytes=0-9\r\n" +
                 "x-amz-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\r\n" +
@@ -39,6 +35,10 @@ public class AmazonSignatureServiceTest {
                         "s3", requestUri, null, null, null, "20130524T000000Z", headersMap, queryParamsMap);
 
         assertEquals(expectedSignature, authorizationHeader.getSignature());
-        assertEquals(expectedAuthorizationHeader, authorizationHeader.getAuthorizationHeader());
+        assertTrue(authorizationHeader.getAuthorizationHeader().contains("Authorization:AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request, SignedHeaders=host;range;x-amz-content-sha256;x-amz-date, Signature=f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41"));
+        assertTrue(authorizationHeader.getAuthorizationHeader().contains("x-amz-content-sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"));
+        assertTrue(authorizationHeader.getAuthorizationHeader().contains("x-amz-date:20130524T000000Z"));
+        assertTrue(authorizationHeader.getAuthorizationHeader().contains("host:examplebucket.s3.amazonaws.com"));
+        assertTrue(authorizationHeader.getAuthorizationHeader().contains("range:bytes=0-9"));
     }
 }
