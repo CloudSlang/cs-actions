74753d41cbe9da4c5d23ef218e245bc776ce3eca
TirlaAlin <alin.tirla@hpe.com>
2016-10-07 10:59:42 +0300

    Fixing merge conflicts

--- cs-jclouds/src/main/java/io/cloudslang/content/jclouds/actions/instances/RebootInstancesAction.java
+++ cs-jclouds/src/main/java/io/cloudslang/content/jclouds/actions/instances/RebootInstancesAction.java
@@ -6,12 +6,11 @@ import com.hp.oo.sdk.content.annotations.Param;
 import com.hp.oo.sdk.content.annotations.Response;
 import com.hp.oo.sdk.content.plugin.ActionMetadata.MatchType;
 import com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType;
-import io.cloudslang.content.jclouds.entities.constants.Constants;
 import io.cloudslang.content.jclouds.entities.constants.Inputs;
 import io.cloudslang.content.jclouds.entities.constants.Outputs;
 import io.cloudslang.content.jclouds.entities.inputs.CommonInputs;
 import io.cloudslang.content.jclouds.entities.inputs.CustomInputs;
-import io.cloudslang.content.jclouds.execute.queries.QueryApiExecutor;
+import io.cloudslang.content.jclouds.execute.instances.RebootInstancesExecutor;
 import io.cloudslang.content.jclouds.utils.ExceptionProcessor;
 
 import java.util.Map;
@@ -21,48 +20,24 @@ import java.util.Map;
  */
 public class RebootInstancesAction {
     /**
-     * Requests a reboot of one or more instances.
-     * Note: This operation is asynchronous; it only queues a request to reboot the specified instances. The operation
-     * succeeds if the instances are valid and belong to you. Requests to reboot terminated instances are ignored.
-     * If an instance does not cleanly shut down within four minutes, Amazon EC2 performs a hard reboot.
-     * For more information about troubleshooting, see Getting Console Output and Rebooting Instances
-     * http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-console.html in the Amazon Elastic Compute Cloud User
-     * Guide.
+     * Requests a reboot of one or more instances. This operation is asynchronous; it only queues a request to reboot
+     * the specified instances. The operation succeeds if the instances are valid and belong to you. Requests to reboot
+     * terminated instances are ignored.
      *
-     * @param endpoint      Endpoint to which request will be sent.
-     *                      Default: "https://ec2.amazonaws.com"
-     * @param identity      ID of the secret access key associated with your Amazon AWS or IAM account.
-     *                      Example: "AKIAIOSFODNN7EXAMPLE"
-     * @param credential    Secret access key associated with your Amazon AWS or IAM account.
-     *                      Example: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-     * @param proxyHost     Optional - proxy server used to connect to Amazon API. If empty no proxy will be used.
-     *                      Default: ""
-     * @param proxyPort     Optional - proxy server port. You must either specify values for both <proxyHost> and <proxyPort>
-     *                      inputs or leave them both empty.
-     *                      Default: ""
-     * @param proxyUsername Optional - proxy server user name.
-     *                      Default: ""
-     * @param proxyPassword Optional - proxy server password associated with the <proxyUsername> input value.
-     *                      Default: ""
-     * @param headers       Optional - string containing the headers to use for the request separated by new line (CRLF).
-     *                      The header name-value pair will be separated by ":".
-     *                      Format: Conforming with HTTP standard for headers (RFC 2616)
-     *                      Examples: "Accept:text/plain"
-     *                      Default: ""
-     * @param queryParams   Optional - string containing query parameters that will be appended to the URL. The names and
-     *                      the values must not be URL encoded because if they are encoded then a double encoded will occur.
-     *                      The separator between name-value pairs is "&" symbol. The query name will be separated from
-     *                      query value by "=".
-     *                      Examples: "parameterName1=parameterValue1&parameterName2=parameterValue2"
-     *                      Default: ""
-     * @param version       Version of the web service to made the call against it.
-     *                      Example: "2016-04-01"
-     * @param delimiter     Optional - delimiter that will be used.
-     *                      Default: ","
-     * @param instanceId    String that contains one or more values that represents instance IDs.
-     *                      Example: "i-12345678,i-abcdef12,i-12ab34cd"
-     * @return A map with strings as keys and strings as values that contains: outcome of the action (or failure message
-     * and the exception if there is one), returnCode of the operation and the ID of the request.
+     * @param provider   Cloud provider on which you have the instances. Valid values: "amazon" or "openstack".
+     * @param endpoint   Endpoint to which request will be sent. Example: "https://ec2.amazonaws.com" for Amazon AWS
+     *                   or "http://hostOrIp:5000/v2.0" for OpenStack.
+     * @param identity   Optional - username of your account or the Access Key ID. For openstack provider the required
+     *                   format is 'alias:username'.
+     * @param credential Optional - password of the user or the Secret Access Key that correspond to the identity input.
+     * @param proxyHost  Optional - proxy server used to access the web site. If empty no proxy will be used.
+     * @param proxyPort  Optional - proxy server port.
+     * @param debugMode  Optional - if "true" then the execution logs will be shown in CLI console.
+     * @param region     Optional - region from which to list servers. Example: "RegionOne", "us-east-1".
+     *                   ListRegionAction operation can be used in order to get all regions - Default: "us-east-1"
+     * @param instanceId Optional - ID of the instance you want to reboot.
+     * @return A map with strings as keys and strings as values that contains: outcome of the action, returnCode of the
+     * operation, or failure message and the exception if there is one
      */
     @Action(name = "Reboot Instances",
             outputs = {
@@ -77,42 +52,34 @@ public class RebootInstancesAction {
                             matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.ERROR)
             }
     )
-    public Map<String, String> rebootInstances(@Param(value = Inputs.CommonInputs.ENDPOINT, required = true) String endpoint,
-                                               @Param(value = Inputs.CommonInputs.IDENTITY, required = true) String identity,
-                                               @Param(value = Inputs.CommonInputs.CREDENTIAL, required = true, encrypted = true) String credential,
-                                               @Param(value = Inputs.CommonInputs.PROXY_HOST) String proxyHost,
-                                               @Param(value = Inputs.CommonInputs.PROXY_PORT) String proxyPort,
-                                               @Param(value = Inputs.CommonInputs.PROXY_USERNAME) String proxyUsername,
-                                               @Param(value = Inputs.CommonInputs.PROXY_PASSWORD, encrypted = true) String proxyPassword,
-                                               @Param(value = Inputs.CommonInputs.HEADERS) String headers,
-                                               @Param(value = Inputs.CommonInputs.QUERY_PARAMS) String queryParams,
-                                               @Param(value = Inputs.CommonInputs.VERSION, required = true) String version,
-                                               @Param(value = Inputs.CommonInputs.DELIMITER, required = true) String delimiter,
+    public Map<String, String> execute(@Param(value = Inputs.CommonInputs.PROVIDER, required = true) String provider,
+                                       @Param(value = Inputs.CommonInputs.ENDPOINT, required = true) String endpoint,
+                                       @Param(value = Inputs.CommonInputs.IDENTITY) String identity,
+                                       @Param(value = Inputs.CommonInputs.CREDENTIAL, encrypted = true) String credential,
+                                       @Param(value = Inputs.CommonInputs.PROXY_HOST) String proxyHost,
+                                       @Param(value = Inputs.CommonInputs.PROXY_PORT) String proxyPort,
+                                       @Param(value = Inputs.CommonInputs.DEBUG_MODE) String debugMode,
 
-                                               @Param(value = Inputs.CustomInputs.INSTANCE_ID, required = true) String instanceId) {
+                                       @Param(value = Inputs.CustomInputs.REGION) String region,
+                                       @Param(value = Inputs.CustomInputs.INSTANCE_ID, required = true) String instanceId) throws Exception {
+
+        CommonInputs inputs = new CommonInputs.Builder()
+                .withProvider(provider)
+                .withEndpoint(endpoint)
+                .withIdentity(identity)
+                .withCredential(credential)
+                .withProxyHost(proxyHost)
+                .withProxyPort(proxyPort)
+                .withDebugMode(debugMode)
+                .build();
+
+        CustomInputs customInputs = new CustomInputs.Builder()
+                .withRegion(region)
+                .withInstanceId(instanceId)
+                .build();
+
         try {
-            CommonInputs commonInputs = new CommonInputs.Builder()
-                    .withEndpoint(endpoint)
-                    .withIdentity(identity)
-                    .withCredential(credential)
-                    .withProxyHost(proxyHost)
-                    .withProxyPort(proxyPort)
-                    .withProxyUsername(proxyUsername)
-                    .withProxyPassword(proxyPassword)
-                    .withHeaders(headers)
-                    .withQueryParams(queryParams)
-                    .withVersion(version)
-                    .withDelimiter(delimiter)
-                    .withAction(Constants.QueryApiActions.REBOOT_INSTANCES)
-                    .withApiService(Constants.Apis.AMAZON_EC2_API)
-                    .withRequestUri(Constants.Miscellaneous.EMPTY)
-                    .withRequestPayload(Constants.Miscellaneous.EMPTY)
-                    .withHttpClientMethod(Constants.AwsParams.HTTP_CLIENT_METHOD_GET)
-                    .build();
-
-            CustomInputs customInputs = new CustomInputs.Builder().withInstanceId(instanceId).build();
-
-            return new QueryApiExecutor().execute(commonInputs, customInputs);
+            return new RebootInstancesExecutor().execute(inputs, customInputs);
         } catch (Exception e) {
             return ExceptionProcessor.getExceptionResult(e);
         }
