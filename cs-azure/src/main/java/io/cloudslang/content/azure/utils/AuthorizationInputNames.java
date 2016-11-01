f515133ab852eba0f97b707ed65c50b875b39fc7
victorursan <victor.ursan@gmail.com>
2016-11-01 19:37:14 +0200

    change authority in loginAuthority

--- cs-azure/src/main/java/io/cloudslang/content/azure/utils/AuthorizationInputNames.java
+++ cs-azure/src/main/java/io/cloudslang/content/azure/utils/AuthorizationInputNames.java
@@ -9,7 +9,7 @@ public final class AuthorizationInputNames extends InputNames {
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
     public static final String CLIENT_ID = "clientId";
-    public static final String AUTHORITY = "authority";
+    public static final String LOGIN_AUTHORITY = "loginAuthority";
     public static final String RESOURCE = "resource";
     public static final String IDENTIFIER = "identifier";
     public static final String PRIMARY_OR_SECONDARY_KEY = "primaryOrSecondaryKey";
