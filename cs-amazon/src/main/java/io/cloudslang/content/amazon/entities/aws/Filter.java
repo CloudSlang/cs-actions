6442fe1b0d7760a83d5bfb4c3b7e3ae259538ed0
Robert Sandor <robert.sandor@hpe.com>
2017-02-16 15:44:59 +0200

    license headers and a new test

--- cs-amazon/src/main/java/io/cloudslang/content/amazon/entities/aws/Filter.java
+++ cs-amazon/src/main/java/io/cloudslang/content/amazon/entities/aws/Filter.java
@@ -1,7 +1,17 @@
+/*******************************************************************************
+ * (c) Copyright 2017 Hewlett-Packard Development Company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *******************************************************************************/
 package io.cloudslang.content.amazon.entities.aws;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -13,7 +23,7 @@ public class Filter {
     private final String name;
     private final List<String> values;
 
-    public Filter(String name, List<String> values) {
+    public Filter(@NotNull final String name, @NotNull final List<String> values) {
         this.name = name;
         this.values = values;
     }
