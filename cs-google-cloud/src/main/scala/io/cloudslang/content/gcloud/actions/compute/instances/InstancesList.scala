9704ab2d1497d26487a68e83978c7e9e89602c06
Victor Ursan <victor.ursan@hpe.com>
2017-02-27 17:23:24 +0200

    first version of InstancesList @Action
    Signed-off-by: Victor Ursan <victor.ursan@hpe.com>

--- cs-google-cloud/src/main/scala/io/cloudslang/content/gcloud/actions/compute/instances/InstancesList.scala
+++ cs-google-cloud/src/main/scala/io/cloudslang/content/gcloud/actions/compute/instances/InstancesList.scala
@@ -1,11 +1,16 @@
 package io.cloudslang.content.gcloud.actions.compute.instances
 
+import java.io.FileInputStream
 import java.util
 
+import com.google.api.services.compute.ComputeScopes
+import com.google.api.services.compute.model.Instance
 import com.hp.oo.sdk.content.annotations.{Action, Output, Param, Response}
 import com.hp.oo.sdk.content.plugin.ActionMetadata.{MatchType, ResponseType}
 import io.cloudslang.content.constants.{OutputNames, ResponseNames, ReturnCodes}
-import io.cloudslang.content.utils.OutputUtilities
+import io.cloudslang.content.gcloud.services.compute.instances.InstanceService
+import io.cloudslang.content.gcloud.utils.{GoogleAuth, HttpTransportUtils, JsonFactoryUtils}
+import io.cloudslang.content.utils.{BooleanUtilities, NumberUtilities, OutputUtilities}
 
 /**
   * Created by victor on 27.02.2017.
@@ -23,9 +28,36 @@ class InstancesList {
       new Response(text = ResponseNames.FAILURE, field = OutputNames.RETURN_CODE, value = ReturnCodes.FAILURE, matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.ERROR, isOnFail = true)
     )
   )
-  def execute(@Param(value = "projectId") nothing: String,
-              @Param(value = "zone") zone: String): util.Map[String, String] = {
+  def execute(@Param(value = "projectId", required = true) projectId: String,
+              @Param(value = "zone", required = true) zone: String,
+              @Param(value = "proxyHost") proxyHost: String,
+              @Param(value = "proxyPort") proxyPort: String,
+              @Param(value = "proxyUsername") proxyUsername: String,
+              @Param(value = "proxyPassword", encrypted = true) proxyPassword: String,
+              @Param(value = "prettyPrint") prettyPrintStr: String,
+              jsonToken: String): util.Map[String, String] = {
+    try {
+      val httpTransport = HttpTransportUtils.getNetHttpTransport(Option(proxyHost), NumberUtilities.toInteger(proxyPort), Option(proxyUsername), proxyPassword)
+      val jsonFactory = JsonFactoryUtils.getDefaultJacksonFactory
 
-    OutputUtilities.getSuccessResultsMap("")
+      val credential = GoogleAuth.fromJsonWithScopes(new FileInputStream(jsonToken), httpTransport, jsonFactory, List(ComputeScopes.COMPUTE_READONLY))
+
+      val prettyPrint = BooleanUtilities.toBoolean(prettyPrintStr)
+
+      val instanceDelimiter = if (prettyPrint) "\n" else ","
+
+      val resultList = InstanceService.list(httpTransport, jsonFactory, credential, projectId, zone)
+        .map { instance: Instance =>
+          if (prettyPrint) {
+            instance.toPrettyString
+          } else {
+            instance.toString
+          }
+        }.mkString("[", instanceDelimiter, "]")
+      OutputUtilities.getSuccessResultsMap(resultList)
+    } catch {
+      case e: Throwable => OutputUtilities.getFailureResultsMap(e)
+    }
   }
+
 }
--- cs-google-cloud/src/main/scala/io/cloudslang/content/gcloud/services/compute/instances/InstanceService.scala
+++ cs-google-cloud/src/main/scala/io/cloudslang/content/gcloud/services/compute/instances/InstanceService.scala
@@ -1,14 +1,10 @@
 package io.cloudslang.content.gcloud.services.compute.instances
 
-import java.io.FileInputStream
-
-import com.google.api.client.auth.oauth2.Credential
+`import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.http.HttpTransport
 import com.google.api.client.json.JsonFactory
-import com.google.api.services.compute.ComputeScopes
 import com.google.api.services.compute.model._
 import io.cloudslang.content.gcloud.services.compute.ComputeService
-import io.cloudslang.content.gcloud.utils.{GoogleAuth, HttpTransportUtils, JsonFactoryUtils}
 
 import scala.collection.JavaConversions._
 
@@ -16,20 +12,6 @@ import scala.collection.JavaConversions._
   * Created by victor on 2/23/17.
   */
 object InstanceService {
-
-  def main(args: Array[String]): Unit = {
-
-    val project = ""
-    val zone = ""
-    val jsonToken = ""
-
-    val httpTransport = HttpTransportUtils.getNetHttpTransport(Some(""), 1)
-    val jsonFactory = JsonFactoryUtils.getDefaultJacksonFactory
-    val credential = GoogleAuth.fromJsonWithScopes(new FileInputStream(jsonToken),
-      httpTransport, jsonFactory, List(ComputeScopes.COMPUTE_READONLY))
-
-    list(httpTransport, jsonFactory, credential, project, zone) foreach print
-  }
 
   def list(httpTransport: HttpTransport, jsonFactory: JsonFactory, credential: Credential, project: String, zone: String): List[Instance] = {
     val computeInstances = ComputeService.instancesService(httpTransport, jsonFactory, credential)
