85b19015f9f4eba59628752502b3cf58207d6af0
Robert Sandor <robert.sandor@hpe.com>
2017-02-28 15:40:58 +0200

    added validation for instanceGet
    Signed-off-by: Robert Sandor <robert.sandor@hpe.com>

--- cs-google-cloud/src/main/scala/io/cloudslang/content/gcloud/actions/compute/instances/InstanceGet.scala
+++ cs-google-cloud/src/main/scala/io/cloudslang/content/gcloud/actions/compute/instances/InstanceGet.scala
@@ -1,25 +1,18 @@
 package io.cloudslang.content.gcloud.actions.compute.instances
 
-import java.io.{File, FileInputStream}
-import java.nio.charset.StandardCharsets
 import java.util
 
-import com.google.api.services.compute.ComputeScopes
 import com.hp.oo.sdk.content.annotations.{Action, Output, Param, Response}
 import com.hp.oo.sdk.content.plugin.ActionMetadata.{MatchType, ResponseType}
 import io.cloudslang.content.constants.{OutputNames, ResponseNames, ReturnCodes}
-import io.cloudslang.content.gcloud.actions.compute.utils.GetAuthorizationToken
 import io.cloudslang.content.gcloud.services.compute.instances.InstanceService
 import io.cloudslang.content.gcloud.utils.InputNames._
-import io.cloudslang.content.gcloud.utils.InputValidator.validate
+import io.cloudslang.content.gcloud.utils.InputValidator.{validateBoolean, validateProxyPort}
 import io.cloudslang.content.gcloud.utils._
 import io.cloudslang.content.utils.{BooleanUtilities, NumberUtilities, OutputUtilities}
-import org.apache.commons.io.IOUtils
-
-import scala.collection.JavaConversions._
 
 /**
-  * Created by sandorr 
+  * Created by sandorr
   * 2/27/2017.
   */
 class InstanceGet {
@@ -37,25 +30,26 @@ class InstanceGet {
   def execute(@Param(value = PROJECT_ID, required = true) projectId: String,
               @Param(value = ZONE, required = true) zone: String,
               @Param(value = INSTANCE_NAME, required = true) instanceName: String,
-              @Param(value = ACCESS_TOKEN) accessToken: String,
+              @Param(value = ACCESS_TOKEN, required = true, encrypted = true) accessToken: String,
               @Param(value = PROXY_HOST) proxyHost: String,
               @Param(value = PROXY_PORT) proxyPort: String,
               @Param(value = PROXY_USERNAME) proxyUsername: String,
               @Param(value = PROXY_PASSWORD, encrypted = true) proxyPassword: String,
-              @Param(value = PRETTY_PRINT) prettyPrintString: String): util.Map[String, String] = {
+              @Param(value = PRETTY_PRINT) prettyPrintStr: String): util.Map[String, String] = {
+    val validationStream = validateProxyPort(proxyPort) ++
+      validateBoolean(prettyPrintStr, PRETTY_PRINT)
+
+    if (validationStream.nonEmpty) {
+      return OutputUtilities.getFailureResultsMap(validationStream.mkString("\n"))
+    }
+
     try {
-      val validationStream = validate(proxyPort)(Option.apply) ++
-        validate(proxyHost)(Option.apply)
-
-      val validationResult = validationStream.mkString("\n")
-      println(validationResult)
-
       val httpTransport = HttpTransportUtils.getNetHttpTransport(Option(proxyHost), NumberUtilities.toInteger(proxyPort), Option(proxyUsername), proxyPassword)
       val jsonFactory = JsonFactoryUtils.getDefaultJacksonFactory
 
       val credential = GoogleAuth.fromAccessToken(accessToken)
 
-      val prettyPrint = BooleanUtilities.toBoolean(prettyPrintString, false)
+      val prettyPrint = BooleanUtilities.toBoolean(prettyPrintStr, false)
 
       val instance = InstanceService.get(httpTransport, jsonFactory, credential, projectId, zone, instanceName)
       val resultString = if (prettyPrint) instance.toPrettyString else instance.toString
--- cs-google-cloud/src/main/scala/io/cloudslang/content/gcloud/utils/InputValidator.scala
+++ cs-google-cloud/src/main/scala/io/cloudslang/content/gcloud/utils/InputValidator.scala
@@ -1,15 +1,28 @@
 package io.cloudslang.content.gcloud.utils
 
+import io.cloudslang.content.utils.BooleanUtilities
+import io.cloudslang.content.utils.OtherUtilities.isValidIpPort
+
 /**
   * Created by sandorr 
   * 2/28/2017.
   */
 object InputValidator {
 
-  def validate(inputValue: String) (validator: (String) => (Option[String])): Stream[String] =
-    validator(inputValue).toStream
+  val INVALID_PORT = "Invalid port value!"
+  val INVALID_BOOLEAN = "Invalid value for prettyPrint input!"
 
-  def proxyPortValidator: (String) => Stream[String] = validate(_) { value =>
-    Option.apply(value)
+  def validateProxyPort: (String) => Stream[String] = validate(_, InputNames.PROXY_PORT) { value =>
+    if (!isValidIpPort(value)) Some(INVALID_PORT) else None
+  }
+
+  def validate(inputValue: String, inputName: String)(validator: (String) => (Option[String])): Stream[String] =
+    validator(inputValue) match {
+      case None => Stream.empty
+      case Some(errorValue) => Stream(s"$inputName : $errorValue")
+    }
+
+  def validateBoolean: (String, String) => Stream[String] = validate(_, _) { value =>
+    if (!BooleanUtilities.isValid(value)) Some(INVALID_BOOLEAN) else None
   }
 }
