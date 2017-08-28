e25dce89c40edd180609ecc7fb7d792297320b95
Iulia Moldovan <iulia.moldovan@hpe.com>
2017-08-28 11:41:50 +0300

    Added a new module which replaces an empty or blank string with a default value
    Signed-off-by: Iulia Moldovan <iulia.moldovan@hpe.com>

--- cs-utilities/src/main/java/io/cloudslang/content/utilities/actions/DefaultIfEmptyAction.java
+++ cs-utilities/src/main/java/io/cloudslang/content/utilities/actions/DefaultIfEmptyAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * (c) Copyright 2017 Hewlett-Packard Development Company, L.P.
+ * (c) Copyright 2017 Hewlett-Packard Enterprise Development Company, L.P.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License v2.0 which accompany this distribution.
  *
@@ -15,16 +15,20 @@ import com.hp.oo.sdk.content.annotations.Param;
 import com.hp.oo.sdk.content.annotations.Response;
 import com.hp.oo.sdk.content.plugin.ActionMetadata.MatchType;
 import com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType;
+import io.cloudslang.content.constants.BooleanValues;
 import io.cloudslang.content.constants.ReturnCodes;
-import io.cloudslang.content.utils.BooleanUtilities;
-import io.cloudslang.content.utils.StringUtilities;
+import io.cloudslang.content.utilities.services.DefaultIfEmptyService;
 
 import java.util.Map;
 
 import static io.cloudslang.content.constants.OutputNames.*;
-import static io.cloudslang.content.constants.ReturnCodes.*;
+import static io.cloudslang.content.constants.ReturnCodes.FAILURE;
+import static io.cloudslang.content.constants.ReturnCodes.SUCCESS;
+import static io.cloudslang.content.utilities.entities.constants.Inputs.*;
+import static io.cloudslang.content.utils.BooleanUtilities.toBoolean;
 import static io.cloudslang.content.utils.OutputUtilities.getFailureResultsMap;
 import static io.cloudslang.content.utils.OutputUtilities.getSuccessResultsMap;
+import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
 
 /**
  * Created by moldovai on 8/21/2017.
@@ -35,20 +39,15 @@ public class DefaultIfEmptyAction {
      * This operation checks if a string is blank or empty and if it's true a default value
      * will be assigned instead of the initial string.
      *
-     * @param initial_value The initial string.
-     * @param default_value The default value used to replace the initial string.
-     * @param trim A variable used to check if the initial string is blank or empty.
+     * @param initialValue The initial string.
+     * @param defaultValue The default value used to replace the initial string.
+     * @param trim         A variable used to check if the initial string is blank or empty.
      * @return a map containing the output of the operation. Keys present in the map are:
-     * <p/>
-     * <br><br><b>returnResult</b> - This will contain the replaced string with the default value.
-     * <br><b>exception</b> - In case of success response, this result is empty. In case of failure response,
+     * returnResult - This will contain the replaced string with the default value.
+     * exception - In case of success response, this result is empty. In case of failure response,
      * this result contains the java stack trace of the runtime exception.
-     * <br><br><b>returnCode</b> - The returnCode of the operation: 0 for success, -1 for failure.
+     * returnCode - The returnCode of the operation: 0 for success, -1 for failure.
      */
-
-    private static final String INITIAL_VALUE = "initial_value";
-    private static final String DEFAULT_VALUE = "default_value";
-    private static final String TRIM = "trim";
 
     @Action(name = "Default value if the initial string is blank or empty",
             outputs = {
@@ -61,27 +60,19 @@ public class DefaultIfEmptyAction {
                     @Response(text = FAILURE, field = RETURN_CODE, value = ReturnCodes.FAILURE, matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.ERROR, isOnFail = true)
             })
     public Map<String, String> execute(
-            @Param(value = INITIAL_VALUE) String initial_value,
-            @Param(value = DEFAULT_VALUE, required = true) String default_value,
+            @Param(value = INITIAL_VALUE) String initialValue,
+            @Param(value = DEFAULT_VALUE, required = true) String defaultValue,
             @Param(value = TRIM) String trim) {
 
         try {
-            trim = StringUtilities.defaultIfBlank(trim, "true");
+            trim = defaultIfBlank(trim, BooleanValues.TRUE);
 
-            boolean validTrim = BooleanUtilities.toBoolean(trim);
+            final boolean validTrim = toBoolean(trim);
 
-            String resultString;
-            if (validTrim) {
-                resultString = StringUtilities.defaultIfBlank(initial_value, default_value);
-            } else {
-                resultString = StringUtilities.defaultIfEmpty(initial_value, default_value);
-            }
-            return getSuccessResultsMap(resultString);
+            return getSuccessResultsMap(DefaultIfEmptyService.defaultIfBlankOrEmpty(initialValue, defaultValue, validTrim));
 
         } catch (Exception e) {
-            final Map<String, String> result = getFailureResultsMap(e);
-            result.put(RETURN_RESULT, "Failure");
-            return result;
+            return getFailureResultsMap(e);
         }
     }
 }
