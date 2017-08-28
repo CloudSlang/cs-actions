804519f989202cbbacfa521b5709eebdde1a464d
sorin <sorin@hpe.com>
2017-08-28 11:28:12 +0300

    Small desc change
    Signed-off-by: sorin <sorin@hpe.com>

--- cs-azure/src/main/java/io/cloudslang/content/azure/utils/DescriptionConstants.java
+++ cs-azure/src/main/java/io/cloudslang/content/azure/utils/DescriptionConstants.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * (c) Copyright 2017 Hewlett-Packard Development Company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *******************************************************************************/
 package io.cloudslang.content.azure.utils;
 
 public class DescriptionConstants {
@@ -11,16 +20,16 @@ public class DescriptionConstants {
         public static final String PASSWORD_DESC = "The password used to authenticate to the Azure Management Service.";
         public static final String CLIENT_ID_DESC = "The client ID.";
         public static final String LOGIN_AUTHORITY_DESC = "URL of the login authority that should be used when retrieving the Authentication Token.\n" +
-                "Default: https://login.windows.net/common";
-        public static final String RESOURCE_DESCRIPTION = " Resource URl for which the Authentication Token is intended.\n" +
-                "Default: https://management.azure.com/";
+                "Default: \"https://login.windows.net/common\"";
+        public static final String RESOURCE_DESCRIPTION = "Resource URl for which the Authentication Token is intended.\n" +
+                "Default: \"https://management.azure.com/\"";
         public static final String PROXY_HOST_DESC = "Proxy server used to access the web site.";
         public static final String PROXY_PORT_DESC = "The port to connect to the proxy server. Enter both proxyHost and proxyPort only if you use proxy server.\n" +
-                "Default: 8080";
+                "Default: \"8080\"";
         public static final String PROXY_USERNAME_DESC = "User name used when connecting to the proxy.\n" +
-                "Default:\"\"";
+                "Default: \"\"";
         public static final String PROXY_PASSWORD_DESC = "The proxy server password associated with the proxyUsername input value.\n" +
-                "Default:\"\" ";
+                "Default: \"\"";
     }
 
     public static class OutputsDescription {
