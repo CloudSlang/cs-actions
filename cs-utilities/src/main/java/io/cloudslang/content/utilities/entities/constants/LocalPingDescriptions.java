91ea3fc1d987a6f26766fe618d87544468d5f89a
moldovso <sorin@hpe.com>
2018-01-18 20:05:27 +0200

    Update description
    Signed-off-by: moldovso <sorin@hpe.com>

--- cs-utilities/src/main/java/io/cloudslang/content/utilities/entities/constants/LocalPingDescriptions.java
+++ cs-utilities/src/main/java/io/cloudslang/content/utilities/entities/constants/LocalPingDescriptions.java
@@ -23,12 +23,10 @@ public class LocalPingDescriptions {
     public static class InputsDescription {
         public static final String IP_VERSION_INPUT_DESC = "IP version forced to the ping command executed on the target host. " +
                 "For Windows -4 or -6 parameters will be added.On Linux will be used ping or ping6. " +
-                "For Solaris -A inet or -A inet6 parameters will be added. " +
-                "For empty string the operation will decide what format to use if targetHost is an ip literal; if targetHost is given as a hostname default 'ping' command will be used on each operating system.\n" +
-                "Values: 4, 6, '' (empty string without quotes).\n" +
-                "Default: '' (empty string without quotes).";
-        public static final String PACKET_SIZE_INPUT_DESC = "The size of the ping packet";
-        public static final String TIMEOUT_INPUT_DESC = "The timeout in miliseconds for the Local Ping operation.\n Default: 90000.";
+                "For empty string the operation will decide what format to use if targetHost is an ip literal; if targetHost " +
+                "is given as a hostname default 'ping' command will be used on each operating system.";
+        public static final String PACKET_SIZE_INPUT_DESC = "The size of the ping packet.";
+        public static final String TIMEOUT_INPUT_DESC = "The timeout in milliseconds for the Local Ping operation.";
         public static final String TARGET_HOST_INPUT_DESC = "The target host to ping.";
         public static final String PACKETS_COUNT_INPUT_DESC = "The number of packets to send.";
     }
