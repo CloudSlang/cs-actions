a765dccaa42607f6de986de59948182f7e19d38b
petmariu <marius.petrovan@hpe.com>
2018-09-03 11:11:22 +0300

    Finish the ListIteratorAction operation
    Signed-off-by: petmariu <marius.petrovan@hpe.com>

--- cs-lists/src/main/java/io/cloudslang/content/actions/ListIteratorAction.java
+++ cs-lists/src/main/java/io/cloudslang/content/actions/ListIteratorAction.java
@@ -1,3 +1,17 @@
+/*
+ * (c) Copyright 2017 EntIT Software LLC, a Micro Focus company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cloudslang.content.actions;
 
 import com.hp.oo.sdk.content.annotations.Action;
@@ -8,29 +22,33 @@ import com.hp.oo.sdk.content.plugin.ActionMetadata.MatchType;
 import com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType;
 import com.hp.oo.sdk.content.plugin.GlobalSessionObject;
 import io.cloudslang.content.services.ListService;
+import io.cloudslang.content.utils.Constants;
 
 import java.util.Map;
 
+import static io.cloudslang.content.utils.Constants.Descriptions.*;
+import static io.cloudslang.content.utils.Constants.OutputNames.*;
+import static io.cloudslang.content.utils.Constants.ResponseNames.FAILURE;
+import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
+
 public class ListIteratorAction {
 
-    /**
-     * This operation is used to iterate a list of values with the help of GlobalSessionObject in order to keep track of the last index. It is not recommended to modify the value of the "list" and "separator" inputs during the iteration process.
-     *
-     * @param list      The list to iterate through.
-     * @param separator A delimiter separating the list elements. This may be single character, multi-characters or special characters.
-     *                  Default value: ‘,’ (comma).
-     * @return resultString The current list element (if the response is "has more").
-     */
-    @Action(name = "List Iterator",
+    @Action(name = "List Iterator", description = LIST_ITERATOR,
             outputs = {
-                    @Output("resultString")},
+                    @Output(value = RESULT_STRING, description = RESULT_STRING_DESC),
+                    @Output(value = RETURN_RESULT, description = RETURN_RESULT_DESC),
+                    @Output(value = RETURN_CODE, description = RETURN_CODE_DESC)},
             responses = {
-                    @Response(text = "has more", field = "result", value = "has more", matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.RESOLVED),
-                    @Response(text = "no more", field = "result", value = "no more", matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.RESOLVED),
-                    @Response(text = "failure", field = "result", value = "failure", matchType = MatchType.COMPARE_EQUAL, isDefault = true, isOnFail = true)})
-    public Map<String, String> execute(@Param("list") String list,
-                                       @Param("separator") String separator,
+                    @Response(text = HAS_MORE, field = RESULT_TEXT, value = HAS_MORE, matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.RESOLVED, description = HAS_MORE_DESC),
+                    @Response(text = NO_MORE, field = RESULT_TEXT, value = NO_MORE, matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.RESOLVED, description = NO_MORE_DESC),
+                    @Response(text = FAILURE, field = RESULT_TEXT, value = FAILURE, matchType = MatchType.COMPARE_EQUAL, isDefault = true, isOnFail = true, description = FAILURE_DESC)})
+    public Map<String, String> execute(@Param(value = LIST, required = true, description = LIST_DESC) String list,
+                                       @Param(value = SEPARATOR, required = true, description = Constants.Descriptions.SEPARATOR_DESC) String separator,
                                        @Param("globalSessionObject") GlobalSessionObject<Map<String, Object>> globalSessionObject) {
-        return ListService.iterate(list, separator, globalSessionObject);
+
+        //Get default values
+        final String separatorImp = defaultIfEmpty(separator, ",");
+
+        return ListService.iterate(list, separatorImp, globalSessionObject);
     }
 }
--- cs-lists/src/test/java/io/cloudslang/content/actions/ListIteratorActionTest.java
+++ cs-lists/src/test/java/io/cloudslang/content/actions/ListIteratorActionTest.java
@@ -1,25 +1,51 @@
+/*
+ * (c) Copyright 2017 EntIT Software LLC, a Micro Focus company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cloudslang.content.actions;
 
 import com.hp.oo.sdk.content.plugin.GlobalSessionObject;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
-import java.util.HashMap;
 import java.util.Map;
+
+import static io.cloudslang.content.utils.Constants.Descriptions.HAS_MORE;
+import static io.cloudslang.content.utils.Constants.Descriptions.NO_MORE;
+import static io.cloudslang.content.utils.Constants.OutputNames.RESULT_STRING;
+import static io.cloudslang.content.utils.Constants.OutputNames.RESULT_TEXT;
+import static org.junit.Assert.assertEquals;
 
 public class ListIteratorActionTest {
 
     private static final String LIST_STRING = "Maria, Ioana, Matei, Gicu, George";
     private static final String LIST_INTEGER = "12, 11, 10, 154, 22, 1";
     private static final String EMPTY_LIST = ",,,";
-    private static final String RETURN_STRING = "returnResult";
 
     @Test
     public void testListIteratorSuccess() {
         final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
         Map<String, String> result = new ListIteratorAction().execute(LIST_STRING, ",", globalSessionObject);
-        assertEquals("has more", result.get("result"));
-        assertEquals("Maria", result.get(RETURN_STRING));
+        assertEquals(HAS_MORE, result.get(RESULT_TEXT));
+        assertEquals("Maria", result.get(RESULT_STRING));
+        assertEquals("0", result.get("index"));
+    }
+
+    @Test
+    public void testListIteratorSuccessFullList() {
+        final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
+        Map<String, String> result = new ListIteratorAction().execute(LIST_STRING, "ceva", globalSessionObject);
+        assertEquals(HAS_MORE, result.get(RESULT_TEXT));
+        assertEquals(LIST_STRING, result.get(RESULT_STRING));
         assertEquals("0", result.get("index"));
     }
 
@@ -27,31 +53,54 @@ public class ListIteratorActionTest {
     public void testListIteratorFails() {
         final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
         Map<String, String> result = new ListIteratorAction().execute(LIST_STRING, "", globalSessionObject);
-        assertEquals("failed", result.get("result"));
-        assertEquals("delimiter has null or 0 length", result.get(RETURN_STRING));
+        assertEquals(HAS_MORE, result.get(RESULT_TEXT));
+        assertEquals("Maria", result.get(RESULT_STRING));
     }
 
     @Test
     public void testListIteratorEmptyList() {
         final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
         Map<String, String> result = new ListIteratorAction().execute("", ";", globalSessionObject);
-        assertEquals("failed", result.get("result"));
-        assertEquals("list has null or 0 length", result.get(RETURN_STRING));
+        assertEquals("failed", result.get(RESULT_TEXT));
+        assertEquals("list has null or 0 length", result.get(RESULT_STRING));
+    }
+
+    @Test
+    public void testListIteratorEmptyListAndDefaultSeparator() {
+        final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
+        Map<String, String> result = new ListIteratorAction().execute("", "", globalSessionObject);
+        assertEquals("failed", result.get(RESULT_TEXT));
+        assertEquals("list has null or 0 length", result.get(RESULT_STRING));
     }
 
     @Test
     public void testListIteratorEmptyListAndSeparator() {
         final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
-        Map<String, String> result = new ListIteratorAction().execute("", "", globalSessionObject);
-        assertEquals("failed", result.get("result"));
-        assertEquals("delimiter has null or 0 length", result.get(RETURN_STRING));
+        Map<String, String> result = new ListIteratorAction().execute(EMPTY_LIST, ",", globalSessionObject);
+        assertEquals(NO_MORE, result.get(RESULT_TEXT));
     }
 
     @Test
-    public void testListIteratorEmptyList2() {
+    public void testListIteratorEmptyListWithDifferentSeparator() {
         final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
-        Map<String, String> result = new ListIteratorAction().execute(EMPTY_LIST, ",", globalSessionObject);
-        assertEquals("no more", result.get("result"));
+        Map<String, String> result = new ListIteratorAction().execute("", ";", globalSessionObject);
+        assertEquals("failed", result.get(RESULT_TEXT));
+        assertEquals("list has null or 0 length", result.get(RESULT_STRING));
     }
 
+    @Test
+    public void testListIteratorIntegerListAndStringSeparator() {
+        final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
+        Map<String, String> result = new ListIteratorAction().execute(LIST_INTEGER, "alabala", globalSessionObject);
+        assertEquals(HAS_MORE, result.get(RESULT_TEXT));
+        assertEquals(LIST_INTEGER, result.get(RESULT_STRING));
+    }
+
+    @Test
+    public void testListIteratorRandomListWordAndSeparator() {
+        final GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
+        Map<String, String> result = new ListIteratorAction().execute("ThisIsOnlyAWord, , , , ,,,", ",", globalSessionObject);
+        assertEquals(HAS_MORE, result.get(RESULT_TEXT));
+        assertEquals("ThisIsOnlyAWord", result.get(RESULT_STRING));
+    }
 }
