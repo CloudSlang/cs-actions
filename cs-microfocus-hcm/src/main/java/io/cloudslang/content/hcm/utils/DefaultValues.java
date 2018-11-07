ef715a0f3a19e6b71190af386f8c2770e613ca6b
mateii <ioana.matei@microfocus.com>
2018-11-07 15:20:54 +0200

    read_component_properties parsing results in random assignment of values - changes were made in defaultValues
    Signed-off-by: mateii <ioana.matei@microfocus.com>

--- cs-microfocus-hcm/src/main/java/io/cloudslang/content/hcm/utils/DefaultValues.java
+++ cs-microfocus-hcm/src/main/java/io/cloudslang/content/hcm/utils/DefaultValues.java
@@ -17,8 +17,8 @@ package io.cloudslang.content.hcm.utils;
 public class DefaultValues {
 
     public static final String DEFAULT_JAVA_KEYSTORE = System.getProperty("java.home") + "/lib/security/cacerts";
-    public static final String DEFAULT_TRUST_ALL_ROOTS = "true";
-    public static final String DEFAULT_X_509_HOSTNAME_VERIFIER = "allow_all";
+    public static final String DEFAULT_TRUST_ALL_ROOTS = "false";
+    public static final String DEFAULT_X_509_HOSTNAME_VERIFIER = "strict";
     public static final String DEFAULT_PROXY_PORT = "8080";
     public static final String DEFAULT_JAVA_KEYSTORE_PASSWORD = "changeit";
 
