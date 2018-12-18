e9d46b883cc102ab2426072a19c51f0b7ea6ba98
graur <liviu.graur@microfocus.com>
2018-12-18 14:50:30 +0200

    Reformat imports
    Signed-off-by: graur <liviu.graur@microfocus.com>

--- cs-office-365/src/main/java/io/cloudslang/content/office365/services/EmailServiceImpl.java
+++ cs-office-365/src/main/java/io/cloudslang/content/office365/services/EmailServiceImpl.java
@@ -24,18 +24,6 @@ import org.apache.http.client.utils.URIBuilder;
 import org.jetbrains.annotations.NotNull;
 
 import static io.cloudslang.content.httpclient.services.HttpClientService.RETURN_RESULT;
-import static io.cloudslang.content.office365.utils.Constants.ANONYMOUS;
-import static io.cloudslang.content.office365.utils.Constants.CHANGEIT;
-import static io.cloudslang.content.office365.utils.Constants.DEFAULT_JAVA_KEYSTORE;
-import static io.cloudslang.content.office365.utils.Constants.GET;
-import static io.cloudslang.content.office365.utils.HttpUtils.getAuthHeaders;
-import static io.cloudslang.content.office365.utils.HttpUtils.getMessagePath;
-import static io.cloudslang.content.office365.utils.HttpUtils.getMessagesPath;
-import static io.cloudslang.content.office365.utils.HttpUtils.getQueryParams;
-import static io.cloudslang.content.office365.utils.HttpUtils.getUriBuilder;
-import static io.cloudslang.content.office365.utils.HttpUtils.setConnectionParameters;
-import static io.cloudslang.content.office365.utils.HttpUtils.setProxy;
-import static io.cloudslang.content.office365.utils.HttpUtils.setSecurityInputs;
 import static io.cloudslang.content.office365.utils.Constants.*;
 import static io.cloudslang.content.office365.utils.HttpUtils.*;
 
