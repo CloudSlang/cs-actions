636416284a93413915b60fca6a178255d80712df
graur <liviu.graur@microfocus.com>
2019-01-30 10:50:45 +0200

    Fix GetUser to use right oDataQuery
    Signed-off-by: graur <liviu.graur@microfocus.com>

--- cs-office-365/src/main/java/io/cloudslang/content/office365/utils/Descriptions.java
+++ cs-office-365/src/main/java/io/cloudslang/content/office365/utils/Descriptions.java
@@ -61,9 +61,9 @@ public class Descriptions {
                 "The input is mutually exclusive with the userId input.";
         public static final String USER_ID_DESC = "The ID of the user to perform the action on.";
         public static final String O_DATA_QUERY_DESC = "Query parameters which can be used to specify and control the amount of data returned " +
-                "in a response specified in '&key1=val1&key2=val2' format. $top and $select options should be not passed for this input because the values" +
+                "in a response specified in 'key1=val1&key2=val2' format. $top and $select options should be not passed for this input because the values" +
                 "for these options can be passed in topQuery and selectQuery inputs." +
-                " Example: &format=json";
+                " Example: $format=json";
         public static final String TOP_QUERY_DESC = "Query parameter use to specify the number of results. Default value: 10";
         public static final String SELECT_QUERY_DESC = "A list of query parameters in the form of a comma delimited list." +
                 " Example: id,internetMessageHeaders";
