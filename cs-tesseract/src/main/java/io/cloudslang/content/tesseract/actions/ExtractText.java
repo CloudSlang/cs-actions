86cf1155588c683d51a18ec48344494bcdb87bed
Ionut Mihai Marisca <ionut-mihai.marisca@microfocus.com>
2019-02-15 13:10:07 +0200

    Initial working POC
    Signed-off-by: Ionut Mihai Marisca <ionut-mihai.marisca@microfocus.com>

---
+++ cs-tesseract/src/main/java/io/cloudslang/content/tesseract/actions/ExtractText.java
@@ -0,0 +1,72 @@
+/*
+ * (c) Copyright 2019 Micro Focus, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.cloudslang.content.tesseract.actions;
+
+import com.hp.oo.sdk.content.annotations.Action;
+import com.hp.oo.sdk.content.annotations.Output;
+import com.hp.oo.sdk.content.annotations.Param;
+import com.hp.oo.sdk.content.annotations.Response;
+import io.cloudslang.content.constants.ReturnCodes;
+
+import java.util.Map;
+
+import static com.hp.oo.sdk.content.plugin.ActionMetadata.MatchType.COMPARE_EQUAL;
+import static com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType.ERROR;
+import static com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType.RESOLVED;
+import static io.cloudslang.content.constants.OutputNames.*;
+import static io.cloudslang.content.constants.ResponseNames.FAILURE;
+import static io.cloudslang.content.constants.ResponseNames.SUCCESS;
+import static io.cloudslang.content.tesseract.services.OcrService.extractText;
+import static io.cloudslang.content.tesseract.utils.Descriptions.Common.EXCEPTION_DESC;
+import static io.cloudslang.content.tesseract.utils.Descriptions.Common.RETURN_CODE_DESC;
+import static io.cloudslang.content.tesseract.utils.Descriptions.ExtractText.*;
+import static io.cloudslang.content.tesseract.utils.Descriptions.InputsDescription.FILE_PATH_DESC;
+import static io.cloudslang.content.tesseract.utils.Inputs.FILE_PATH;
+import static io.cloudslang.content.utils.OutputUtilities.getFailureResultsMap;
+import static io.cloudslang.content.utils.OutputUtilities.getSuccessResultsMap;
+
+public class ExtractText {
+    /**
+     * This operation extracts the text from a specified file given as input using Tesseract's OCR library.
+     *
+     * @param filePath The path of the file to be extracted.
+     *                 Required
+     * @return a map containing the output of the operation. Keys present in the map are:
+     * returnResult - This will contain the extracted text.
+     * exception - In case of success response, this result is empty. In case of failure response,
+     * this result contains the java stack trace of the runtime exception.
+     * returnCode - The returnCode of the operation: 0 for success, -1 for failure.
+     */
+
+    @Action(name = "Extract text",
+            description = EXTRACT_TEXT_DESC,
+            outputs = {
+                    @Output(value = RETURN_CODE, description = RETURN_CODE_DESC),
+                    @Output(value = RETURN_RESULT, description = RETURN_RESULT_DESC),
+                    @Output(value = EXCEPTION, description = EXCEPTION_DESC),
+            },
+            responses = {
+                    @Response(text = SUCCESS, field = RETURN_CODE, value = ReturnCodes.SUCCESS, matchType = COMPARE_EQUAL, responseType = RESOLVED, description = SUCCESS_DESC),
+                    @Response(text = FAILURE, field = RETURN_CODE, value = ReturnCodes.FAILURE, matchType = COMPARE_EQUAL, responseType = ERROR, isOnFail = true, description = FAILURE_DESC)
+            })
+    public Map<String, String> execute(
+            @Param(value = FILE_PATH, required = true, description = FILE_PATH_DESC) String filePath) {
+        try {
+            return getSuccessResultsMap(extractText(filePath));
+        } catch (Exception e) {
+            return getFailureResultsMap(e);
+        }
+    }
+}
