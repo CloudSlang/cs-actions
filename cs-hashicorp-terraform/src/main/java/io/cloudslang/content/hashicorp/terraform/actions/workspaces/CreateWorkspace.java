5efcc14cb51d171a1d69952c7bc313758b46f309
patmante <mantesh.patil@microfocus.com>
2019-11-27 19:43:37 +0530

    Adding CreateWorkspace operation related classes

--- cs-hashicorp-terraform/src/main/java/io/cloudslang/content/hashicorp/terraform/actions/workspaces/CreateWorkspace.java
+++ cs-hashicorp-terraform/src/main/java/io/cloudslang/content/hashicorp/terraform/actions/workspaces/CreateWorkspace.java
@@ -56,6 +56,62 @@ import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
 
 public class CreateWorkspace {
+
+    /**
+     * List An OAuth Client Id which represents  the connection between an organization and a VCS provider.
+     *
+     * @param authToken         required - authentication token used to connect to Terraform API.
+     *
+     * @param organizationName  required - name of the Terraform organization.
+     *
+     * @param workspaceName     Optional - The name of workspace to be created.
+     *
+     * @param vcsRepoId         Optional - The ID of VCS(GIT) repository.
+     *
+     * @param oauthTokenId      Optional - Id of the oauth-token-id of the VCS Provider.
+     *
+     * @param reuestBody        Optional - The request body of the workspace.
+     *
+     * @param proxyHost         Optional - proxy server used to connect to Terraform API. If empty no proxy will be used.
+     *                          Default: ""
+     * @param proxyPort         Optional - proxy server port. You must either specify values for both proxyHost and
+     *                          proxyPort inputs or leave them both empty.
+     *                          Default: ""
+     * @param proxyUsername     Optional - proxy server user name.
+     *                          Default: ""
+     * @param proxyPassword     Optional - proxy server password associated with the proxyUsername input value.
+     *                          Default: ""
+     * @param trustAllRoots     Optional - Specifies whether to enable weak security over SSL/TSL.
+     *
+     * @param x509HostnameVerifier Optional - Specifies the way the server hostname must match a domain name in
+     *                                         the subject's Common Name (CN) or subjectAltName field of the X.509 certificate. Set this to
+     *                                         allow_all to skip any checking. For the value browser_compatible the hostname verifier
+     *                                         works the same way as Curl and Firefox. The hostname must match either the first CN, or any of
+     *                                         the subject-alts. A wildcard can occur in the CN, and in any of the subject-alts. The only
+     *                                         difference between browser_compatible and strict is that a wildcard (such as *.foo.com)
+     *                                         with browser_compatible matches all subdomains, including a.b.foo.com
+     * @param trustKeystore    Optional - The pathname of the Java TrustStore file. This contains certificates from other parties that you expect to communicate with, or from Certificate Authorities
+     *                                    that you trust to identify other parties.  If the protocol (specified by the 'url') is not 'https'
+     *                                    or if trustAllRoots is 'true' this input is ignored. Format: Java KeyStore (JKS);
+     * @param trustPassword    Optional - The password associated with the TrustStore file. If trustAllRoots is false and trustKeystore is empty, trustPassword default will be supplied
+     *
+     * @param connectTimeout   Optional - The time to wait for a connection to be established in seconds. A timeout value of '0' represents an infinite timeout
+     *
+     * @param socketTimeout    Optional - The timeout for waiting for data (a maximum period " +
+     *                                    inactivity between two consecutive data packets), in seconds. A socketTimeout value of '0' represents an infinite timeout
+     * @param keepAlive        Optional - Specifies whether to create a shared connection that will be used in subsequent calls. If keepAlive is false, the already open connection will be used and after" +
+     *                                    execution it will close it
+     * @param connectionsMaxPerRoute Optional - The maximum limit of connections on a per route basis
+     *
+     * @param connectionsMaxTotal    Optional - The maximum limit of connections in total
+     *
+     * @param responseCharacterSet   Optional - The character encoding to be used for the HTTP response. If responseCharacterSet is empty, the charset from the 'Content-Type' HTTP response header will be used.If responseCharacterSet is empty and the charset from the HTTP response Content-Type header is empty, the " +
+     *                                          default value will be used. You should not use this for method=HEAD or OPTIONS.
+     *                               Default : UTF-8
+     * @return                  A map with strings as keys and strings as values that contains: outcome of the action, returnCode of the
+     *                          operation, or failure message and the exception if there is one
+     */
+
     @Action(name = CREATE_WORKSPACE_OPERATION_NAME,
             description = CREATE_WORKSPACE_DESC,
             outputs = {
