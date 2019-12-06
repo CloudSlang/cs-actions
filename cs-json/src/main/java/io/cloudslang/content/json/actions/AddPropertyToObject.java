573837f77ca9204d477b771941cd0a4f473ffd55
EAndreica <andreica_eric@yahoo.com>
2019-12-06 14:21:36 +0200

    Refactor addPropertyToJson operation.

--- cs-json/src/main/java/io/cloudslang/content/json/actions/AddPropertyToObject.java
+++ cs-json/src/main/java/io/cloudslang/content/json/actions/AddPropertyToObject.java
@@ -17,27 +17,32 @@
 package io.cloudslang.content.json.actions;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ContainerNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.hp.oo.sdk.content.annotations.Action;
 import com.hp.oo.sdk.content.annotations.Output;
 import com.hp.oo.sdk.content.annotations.Param;
 import com.hp.oo.sdk.content.annotations.Response;
 import com.hp.oo.sdk.content.plugin.ActionMetadata.MatchType;
 import com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType;
-import io.cloudslang.content.constants.OutputNames;
-import io.cloudslang.content.constants.ResponseNames;
 import io.cloudslang.content.constants.ReturnCodes;
-import io.cloudslang.content.json.utils.Constants;
+import io.cloudslang.content.json.entities.AddPropertyToObjectInputs;
+import io.cloudslang.content.json.services.AddPropertyToObjectImpl;
 import io.cloudslang.content.utils.StringUtilities;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static io.cloudslang.content.json.utils.JsonUtils.populateResult;
+import static io.cloudslang.content.constants.OutputNames.*;
+import static io.cloudslang.content.constants.ResponseNames.FAILURE;
+import static io.cloudslang.content.constants.ResponseNames.SUCCESS;
+import static io.cloudslang.content.json.utils.Constants.AddPropertyToObject.NEW_LINE;
+import static io.cloudslang.content.json.utils.Constants.InputNames.*;
+import static io.cloudslang.content.json.utils.Descriptions.AddPropertyToObject.*;
+import static io.cloudslang.content.json.utils.InputsValidation.verifyJsonObject;
+import static io.cloudslang.content.utils.OutputUtilities.getFailureResultsMap;
+import static io.cloudslang.content.utils.OutputUtilities.getSuccessResultsMap;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
 
 public class AddPropertyToObject {
 
@@ -50,7 +55,7 @@ public class AddPropertyToObject {
     /**
      * Inserts a new name/value property into a JSON object, where the value is a string.
      * This operation forces the value of the name/vale pair to be a string, no matter what data type it actually is.
-     * This means that the <b>newPropertyValue</b> input does not need to be wrapped in quotes as any quotes entered in this
+     * This means that the newPropertyValue input does not need to be wrapped in quotes as any quotes entered in this
      * input will be escaped, ending up in the properties' value.
      * In case that a new property with the same name as an existing one is added, the old property's value will be overwritten.
      * The 'Add JSON Property to Object' operation should be used to add values of type other than string,
@@ -65,102 +70,49 @@ public class AddPropertyToObject {
      * @param newPropertyValue The value for the new property. This is interpreted as a string, no matter what the contents of the input.
      *                         Examples: value, 1, [1,2,3]
      * @return a map containing the output of the operation. Keys present in the map are:
-     * <p/>
-     * <br><br><b>returnResult</b> - This will contain the JSON with the new property/value added.
-     * <br><b>exception</b> - In case of success response, this result is empty. In case of failure response,
-     * this result contains the java stack trace of the runtime exception.
-     * <br><br><b>returnCode</b> - The returnCode of the operation: 0 for success, -1 for failure.
+     * returnResult - This will contain the JSON with the new property/value added.
+     * exception - In case of success response, this result is empty. In case of failure response this result contains
+     * the java stack trace of the runtime exception.
+     * returnCode - The returnCode of the operation: 0 for success, -1 for failure.
      */
-    @Action(name = "Add Property to Object",
+    @Action(name = ADD_PROPERTY_TO_OBJECT,
             outputs = {
-                    @Output(OutputNames.RETURN_RESULT),
-                    @Output(OutputNames.RETURN_CODE),
-                    @Output(OutputNames.EXCEPTION)
+                    @Output(value = RETURN_RESULT, description = RETURN_RESULT_DESC),
+                    @Output(value = RETURN_CODE, description = RETURN_CODE_DESC),
+                    @Output(value = EXCEPTION, description = EXCEPTION_DESC)
             },
             responses = {
-                    @Response(text = ResponseNames.SUCCESS, field = OutputNames.RETURN_CODE, value = ReturnCodes.SUCCESS, matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.RESOLVED),
-                    @Response(text = ResponseNames.FAILURE, field = OutputNames.RETURN_CODE, value = ReturnCodes.FAILURE, matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.ERROR, isOnFail = true)
+                    @Response(text = SUCCESS, field = RETURN_CODE, value = ReturnCodes.SUCCESS, matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.RESOLVED, description = SUCCESS_DESC),
+                    @Response(text = FAILURE, field = RETURN_CODE, value = ReturnCodes.FAILURE, matchType = MatchType.COMPARE_EQUAL, responseType = ResponseType.ERROR, description = FAILURE_DESC, isOnFail = true)
             }
     )
     public Map<String, String> execute(
-            @Param(value = Constants.InputNames.JSON_OBJECT, required = true) String jsonObject,
-            @Param(value = Constants.InputNames.NEW_PROPERTY_NAME) String newPropertyName,
-            @Param(value = Constants.InputNames.NEW_PROPERTY_VALUE) String newPropertyValue) {
+            @Param(value = JSON_OBJECT, description = JSON_OBJECT_DESC, required = true) String jsonObject,
+            @Param(value = NEW_PROPERTY_NAME, description = NEW_PROPERTY_NAME_DESC) String newPropertyName,
+            @Param(value = NEW_PROPERTY_VALUE, description = NEW_PROPERTY_VALUE_DESC) String newPropertyValue) {
 
-        Map<String, String> returnResult = new HashMap<>();
-        if (!validateInput(jsonObject, newPropertyName, newPropertyValue, returnResult)) {
-            return returnResult;
-        }
-        doExecute(jsonObject, newPropertyName, newPropertyValue, returnResult);
-        return returnResult;
-    }
+        newPropertyName = defaultIfEmpty(newPropertyName, EMPTY);
+        newPropertyValue = defaultIfEmpty(newPropertyValue, EMPTY);
 
+        final List<String> exceptionMessages = verifyJsonObject(jsonObject, objectMapper);
 
-    /**
-     * Validates the input for this action.
-     *
-     * @param jsonObject       The value of the <b>jsonObject</b> input.
-     * @param newPropertyName  The value of the <b>newPropertyName</b> input.
-     * @param newPropertyValue The value of the <b>newPropertyValue</b> input.
-     * @param returnResult     The map that will be used as return result for this action.
-     * @return True if input is valid, false otherwise.
-     */
-    private boolean validateInput(String jsonObject, String newPropertyName, String newPropertyValue,
-                                  Map<String, String> returnResult) {
-        return validateJsonObject(jsonObject, returnResult);
-    }
-
-    /**
-     * Validates the <b>jsonObject</b> input.
-     *
-     * @param jsonObject   The value of the <b>jsonObject</b> input.
-     * @param returnResult The map that will be used as return result for this action.
-     * @return True if value of <b>jsonObject</b> is valid, false otherwise.
-     */
-    private boolean validateJsonObject(String jsonObject, Map<String, String> returnResult) {
-        if (StringUtilities.isBlank(jsonObject)) {
-            final String exMsg = "Empty JSON string";
-            populateResult(returnResult, new Exception(exMsg));
-            return false;
+        if (!exceptionMessages.isEmpty()) {
+            return getFailureResultsMap(StringUtilities.join(exceptionMessages, NEW_LINE));
         }
 
         try {
-            JsonNode jsonRoot = objectMapper.readTree(jsonObject);
-            if (jsonRoot == null || !(jsonRoot.isContainerNode() && jsonRoot.isObject())) {
-                throw new Exception();
-            }
+            final AddPropertyToObjectInputs addPropertyToObjectInputs = AddPropertyToObjectInputs.builder()
+                    .jsonObject(jsonObject)
+                    .newPropertyName(newPropertyName)
+                    .newPropertyValue(newPropertyValue)
+                    .build();
+
+            final String resultJson = AddPropertyToObjectImpl.addPropertyToObject(addPropertyToObjectInputs, objectMapper);
+
+            return getSuccessResultsMap(resultJson);
         } catch (Exception exception) {
-            final String exMsg = Constants.InputNames.JSON_OBJECT + " is not a valid JSON Object";
-            populateResult(returnResult, new Exception(exMsg));
-            return false;
-        }
+            return getFailureResultsMap(exception);
 
-        return true;
-    }
-
-    /**
-     * Does the actual logic for this Action
-     *
-     * @param jsonObject       The value of the <b>jsonObject</b> input.
-     * @param newPropertyName  The value of the <b>newPropertyName</b> input.
-     * @param newPropertyValue The value of the <b>newPropertyValue</b> input.
-     * @param returnResult     The map that will be used as return result for this action.
-     */
-    private void doExecute(String jsonObject, String newPropertyName, String newPropertyValue,
-                           Map<String, String> returnResult) {
-        try {
-            JsonNode jsonRoot = objectMapper.readTree(jsonObject);
-            if(!(jsonRoot instanceof ObjectNode)){
-                throw new Exception("The value could not be added!");
-            }
-            ContainerNode jsonResult = ((ObjectNode) jsonRoot).put(newPropertyName, newPropertyValue);
-            if (jsonResult == null) {
-                throw new Exception("The value could not be added!");
-            }
-            populateResult(returnResult, jsonResult.toString(), null);
-        } catch (Exception ex) {
-            populateResult(returnResult, ex);
         }
     }
-
 }
---
+++ cs-json/src/main/java/io/cloudslang/content/json/utils/InputsValidation.java
@@ -0,0 +1,48 @@
+/*
+ * (c) Copyright 2019 EntIT Software LLC, a Micro Focus company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.cloudslang.content.json.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cloudslang.content.utils.StringUtilities;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.cloudslang.content.json.utils.Constants.AddPropertyToObject.EMPTY_JSON;
+import static io.cloudslang.content.json.utils.Constants.AddPropertyToObject.JSON_EXCEPTION;
+
+public class InputsValidation {
+
+    @NotNull
+    public static List<String> verifyJsonObject(@NotNull final String jsonObject, @NotNull ObjectMapper objectMapper) {
+        final List<String> exceptionMessages = new ArrayList<>();
+        if (StringUtilities.isBlank(jsonObject)) {
+            exceptionMessages.add(EMPTY_JSON);
+        }
+        try {
+            JsonNode jsonRoot = objectMapper.readTree(jsonObject);
+            if (jsonRoot == null || !(jsonRoot.isContainerNode() && jsonRoot.isObject())) {
+                throw new Exception();
+            }
+        } catch (Exception exception) {
+            exceptionMessages.add(JSON_EXCEPTION);
+        }
+        return exceptionMessages;
+
+    }
+}
