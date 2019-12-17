6005aad96b0698d82fd36532e7c69d2c6878928d
Manjoosh-Kalaghatagi <manjoosh.kalaghatagi@microfocus.com>
2019-12-17 15:10:14 +0530

    removed un used imports

--- cs-hashicorp-terraform/src/main/java/io/cloudslang/content/hashicorp/terraform/actions/variables/DeleteVariable.java
+++ cs-hashicorp-terraform/src/main/java/io/cloudslang/content/hashicorp/terraform/actions/variables/DeleteVariable.java
@@ -26,8 +26,6 @@ import static io.cloudslang.content.hashicorp.terraform.utils.Constants.Common.N
 import static io.cloudslang.content.hashicorp.terraform.utils.Constants.DeleteVariableConstants.DELETE_VARIABLE_OPERATION_NAME;
 import static io.cloudslang.content.hashicorp.terraform.utils.Descriptions.Common.*;
 import static io.cloudslang.content.hashicorp.terraform.utils.Descriptions.Common.RESPONSE_CHARACTER_SET_DESC;
-
-import static io.cloudslang.content.hashicorp.terraform.utils.Descriptions.CreateWorkspace.SUCCESS_DESC;
 import static io.cloudslang.content.hashicorp.terraform.utils.Descriptions.DeleteVariable.DELETE_VARIABLE_DESC;
 import static io.cloudslang.content.hashicorp.terraform.utils.Descriptions.DeleteVariable.DELETE_VAR_SUCCESS_DESC;
 import static io.cloudslang.content.hashicorp.terraform.utils.Descriptions.UpdateVariable.VARIABLE_ID_DESC;
