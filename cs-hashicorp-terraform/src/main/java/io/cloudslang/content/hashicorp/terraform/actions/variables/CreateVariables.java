8e361c39f58a4590e2d54ca54c949fe4af148d73
soumyanayak71 <soumyanayak71@gmail.com>
2020-01-10 00:42:54 +0530

    Update CreateVariables.java

--- cs-hashicorp-terraform/src/main/java/io/cloudslang/content/hashicorp/terraform/actions/variables/CreateVariables.java
+++ cs-hashicorp-terraform/src/main/java/io/cloudslang/content/hashicorp/terraform/actions/variables/CreateVariables.java
@@ -140,7 +140,7 @@ public class CreateVariables {
 
             final Map<String,String> results=new HashMap<>();
             results.put(RETURN_RESULT, getVariablesOperationOutput(variablesJson,sensitiveVariablesJson,result).toString());
-            esults.put(RETURN_CODE,getVariablesOperationOutput(variablesJson,sensitiveVariablesJson,result).get(RETURN_CODE));
+            results.put(RETURN_CODE,getVariablesOperationOutput(variablesJson,sensitiveVariablesJson,result).get(RETURN_CODE));
             results.put(STATUS_CODE,getVariablesOperationOutput(variablesJson,sensitiveVariablesJson,result).get(STATUS_CODE));
             return results;
 
