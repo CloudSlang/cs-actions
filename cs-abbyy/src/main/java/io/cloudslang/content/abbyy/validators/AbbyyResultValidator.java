432f5d357923e43bd7d931ae962b412dd2374744
Norberth Csorba <norberth.csorba@microfocus.com>
2020-04-16 15:56:54 +0300

    updated license header

--- cs-abbyy/src/main/java/io/cloudslang/content/abbyy/validators/AbbyyResultValidator.java
+++ cs-abbyy/src/main/java/io/cloudslang/content/abbyy/validators/AbbyyResultValidator.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2019 EntIT Software LLC, a Micro Focus company, L.P.
+ * (c) Copyright 2020 EntIT Software LLC, a Micro Focus company, L.P.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License v2.0 which accompany this distribution.
  *
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.cloudslang.content.abbyy.validators;
 
 import io.cloudslang.content.abbyy.exceptions.AbbyySdkException;
