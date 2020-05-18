caf5c5fda815e61cd4e914a2d4ba0b4376c945fc
rajesh-kum-kalyankar <rajesh.kum.kalyankar@microfocus.com>
2020-05-18 14:51:07 +0530

    remove nutanix module

--- cs-nutanix/src/main/java/io/cloudslang/content/nutanix/exceptions/CounterImplException.java
+++
@@ -1,9 +0,0 @@
-package io.cloudslang.content.nutanix.exceptions;
-
-public class CounterImplException extends Exception {
-    private static final long serialVersionUID = -2928111219772269559L;
-
-    public CounterImplException(String e) {
-        super(e);
-    }
-}
