9a3684b2f476f9a8c2351110b070ccaf926c9299
Norberth Csorba <norberth.csorba@microfocus.com>
2020-07-06 15:13:55 +0300

    escaped ESC= in SSH Command

--- cs-ssh/src/main/java/io/cloudslang/content/ssh/utils/AnsiEscapeUtils.java
+++ cs-ssh/src/main/java/io/cloudslang/content/ssh/utils/AnsiEscapeUtils.java
@@ -20,6 +20,7 @@ import org.jetbrains.annotations.Nullable;
 
 public final class AnsiEscapeUtils {
 
+    private static final String ESCAPE_CHAR = "\\x1B";
     private static final String CONTROL_SEQUENCE_INTRODUCER= "\\x9B|\\x1B\\[";
     private static final String PARAMETER_BYTES = "0-9:;<=>?";
     private static final String INTERMEDIATE_BYTES = "!\"#$%&'()*+,\\-./";
@@ -31,12 +32,22 @@ public final class AnsiEscapeUtils {
 
 
     public static String removeEscapeSequences(@Nullable String str) {
-        return str == null ? null : removeControlSequences(str);
+        if(str == null) {
+            return null;
+        }
+        str = removeControlSequences(str);
+        str = removeEscapeEqualChar(str);
+        return str;
     }
 
 
     private static String removeControlSequences(@NotNull String str) {
         String regex = String.format("(%s)[%s]*[%s]*[%s]", CONTROL_SEQUENCE_INTRODUCER, PARAMETER_BYTES, INTERMEDIATE_BYTES, FINAL_BYTES);
+        return str.replaceAll(regex, StringUtils.EMPTY);
+    }
+
+    private static String removeEscapeEqualChar(@NotNull String str) {
+        String regex = ESCAPE_CHAR + "=";
         return str.replaceAll(regex, StringUtils.EMPTY);
     }
 }
