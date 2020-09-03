62833d8119cd0f10a6375e5aa30ccd7b6251ae5a
mateii <ioana.matei@microfocus.com>
2020-09-03 13:48:19 +0300

    changed value for inputName value

--- cs-lists/src/main/java/io/cloudslang/content/utils/InputNames.java
+++ cs-lists/src/main/java/io/cloudslang/content/utils/InputNames.java
@@ -18,7 +18,7 @@ public final class InputNames {
 
     public static final String LIST = "list";
     public static final String DELIMITER = "delimiter";
-    public static final String VALUE = "elements";
+    public static final String VALUE = "value";
     public static final String METHOD = "method";
     public static final String STRIP_WHITESPACES = "stripWhitespaces";
     static final String CS_STRIP_WHITESPACES = "strip_whitespaces";
