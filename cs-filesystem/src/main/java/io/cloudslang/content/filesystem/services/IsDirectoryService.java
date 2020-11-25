ed6dbbb7e1b7ebed50692f6ce929ded8521e7290
Sergiu Ioan.Harjoc <SergiuIoan.Harjoc@microfocus.com>
2020-11-25 13:05:30 +0200

    Add Copyright header

--- cs-filesystem/src/main/java/io/cloudslang/content/filesystem/services/IsDirectoryService.java
+++ cs-filesystem/src/main/java/io/cloudslang/content/filesystem/services/IsDirectoryService.java
@@ -1,5 +1,20 @@
+/*
+ * (c) Copyright 2020 EntIT Software LLC, a Micro Focus company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cloudslang.content.filesystem.services;
 
+import io.cloudslang.content.filesystem.constants.Constants;
 import io.cloudslang.content.filesystem.entities.IsDirectoryInputs;
 import io.cloudslang.content.utils.OutputUtilities;
 import org.jetbrains.annotations.NotNull;
@@ -14,17 +29,9 @@ import static io.cloudslang.content.filesystem.utils.Utils.validateIsDirectory;
 public class IsDirectoryService {
 
     public @NotNull Map<String,String> execute (@NotNull IsDirectoryInputs inputs) throws Exception{
-        File source = new File(inputs.getSource().trim());
-        validateIsDirectory(source);
-        return OutputUtilities.getSuccessResultsMap("Source is a directory");
+        String path = inputs.getSource().trim();
+        File file = new File(path);
+        validateIsDirectory(file,path);
+        return OutputUtilities.getSuccessResultsMap(String.format(Constants.IS_DIRECTORY_SUCCESS,inputs.getSource().trim()));
     }
-
-//    public void validateSource(File source) throws Exception{
-//        if(!source.isAbsolute())
-//            throw new IllegalArgumentException(INVALID_ABSOLUTE_PATH);
-//        if(!source.exists())
-//            throw new FileNotFoundException(DOES_NOT_EXIST);
-//        if(!source.isDirectory())
-//            throw new Exception(NOT_A_DIRECTORY);
-//    }
 }
--- cs-filesystem/src/main/java/io/cloudslang/content/filesystem/utils/Utils.java
+++ cs-filesystem/src/main/java/io/cloudslang/content/filesystem/utils/Utils.java
@@ -1,3 +1,17 @@
+/*
+ * (c) Copyright 2020 EntIT Software LLC, a Micro Focus company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cloudslang.content.filesystem.utils;
 
 import java.io.File;
@@ -7,12 +21,12 @@ import static io.cloudslang.content.filesystem.constants.ExceptionMsgs.*;
 
 public class Utils {
 
-    public static void validateIsDirectory(File source) throws Exception{
+    public static void validateIsDirectory(File source, String path) throws Exception{
         if(!source.isAbsolute())
             throw new IllegalArgumentException(INVALID_ABSOLUTE_PATH);
         if(!source.exists())
-            throw new FileNotFoundException(DOES_NOT_EXIST);
+            throw new FileNotFoundException(String.format(DOES_NOT_EXIST,path));
         if(!source.isDirectory())
-            throw new Exception(NOT_A_DIRECTORY);
+            throw new Exception(String.format(NOT_A_DIRECTORY,path));
     }
 }
