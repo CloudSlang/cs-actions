44f0d4a50a28576f51e5af89e5a2a18c32430d03
Florin Anca <florin.anca@microfocus.com>
2020-12-11 02:00:16 +0200

    Added Copyright and removed required from the compare_to input

--- cs-filesystem/src/main/java/io/cloudslang/content/filesystem/actions/MD5SumAction.java
+++ cs-filesystem/src/main/java/io/cloudslang/content/filesystem/actions/MD5SumAction.java
@@ -1,3 +1,17 @@
+/*
+ * (c) Copyright 2020 EntIT Software LLC, a Micro Focus company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cloudslang.content.filesystem.actions;
 
 import com.hp.oo.sdk.content.annotations.Action;
@@ -35,7 +49,7 @@ public class MD5SumAction {
                     @Response(text = ResponseNames.FAILURE, field = RETURN_CODE, value = ReturnCodes.FAILURE, matchType = MatchType.COMPARE_EQUAL, isOnFail = true, isDefault = true)
             })
     public Map<String,String> execute(@Param(value = InputNames.SOURCE, description = SOURCE_DESCRIPTION, required = true) String source,
-                                      @Param(value = InputNames.COMPARE_TO, description = COMPARE_TO_DESCRIPTION, required = true) String compareTo){
+                                      @Param(value = InputNames.COMPARE_TO, description = COMPARE_TO_DESCRIPTION) String compareTo){
 
 
         try{
