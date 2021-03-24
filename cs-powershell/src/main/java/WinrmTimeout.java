a3017840f67f2db49fd56fd89fff4a8482052ab7
TodereanLenuta <biltiu.lenuta@gmail.com>
2021-03-24 12:05:16 +0200

    Added the dependency for winrm4j and modified the timeout mechanism.

---
+++ cs-powershell/src/main/java/WinrmTimeout.java
@@ -0,0 +1,46 @@
+import io.cloudsoft.winrm4j.client.WinRmClientContext;
+import io.cloudsoft.winrm4j.winrm.WinRmTool;
+import io.cloudsoft.winrm4j.winrm.WinRmToolResponse;
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.http.client.config.AuthSchemes;
+
+public class WinrmTimeout {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WinRmClientContext context = WinRmClientContext.newInstance();
+
+        final WinRmTool tool = WinRmTool.Builder.builder("ps7-win.ros.swinfra.net", "Administrator", "B33f34t3r")
+                .authenticationScheme(AuthSchemes.NTLM)
+                .disableCertificateChecks(true)
+                .port(5985)
+                .useHttps(false)
+                .context(context)
+                .build();
+
+        long userTimeout = 10l;
+
+        StopWatch watch = new StopWatch();
+        watch.start();
+        WinRmToolResponse res = tool.executePs("sleep 10");
+        watch.stop();
+        long result = watch.getTime();
+
+        if (result > userTimeout) {
+
+            context.shutdown();
+            Thread.currentThread().stop();
+
+            //Thread.currentThread().interrupt();
+            //System.out.println("Is Thread interrupted ?" + Thread.currentThread().isInterrupted());
+
+            throw new RuntimeException("timeout");
+        } else
+            context.shutdown();
+
+        System.out.println("StatusCode: " + res.getStatusCode());
+        System.out.println("StdErr: " + res.getStdErr());
+        System.out.println("Stdout: " + res.getStdOut());
+    }
+
+}
