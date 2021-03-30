dfe57741547993bd7cef9194fd53756df3ab4934
boicu <boicu@ROBOICU03>
2021-03-30 17:08:17 +0300

    refactor

--- cs-winrm/src/main/java/io/cloudslang/content/winrm/utils/Constants.java
+++ cs-winrm/src/main/java/io/cloudslang/content/winrm/utils/Constants.java
@@ -12,11 +12,10 @@ public final class Constants {
     public static final String DEFAULT_JAVA_KEYSTORE = System.getProperty("java.home") + "/lib/security/cacerts";
     public static final String CHANGE_IT = "changeit";
     public static final String EXCEPTION_NULL_EMPTY = "The %s can't be null or empty.";
-    public static final String EXCEPTION_INVALID_PATH = "%s for %s is an invalid certificate path.";
+    public static final String EXCEPTION_INVALID_PATH = "The certificate at path %s for %s input does not exists.";
     public static final String EXCEPTION_INVALID_BOOLEAN = "The %s for %s input is not a valid boolean value.";
     public static final String EXCEPTION_INVALID_NUMBER = "The %s for %s input is not a valid number value.";
     public static final String EXCEPTION_INVALID_AUTH_TYPE = "The %s for %s input is not a valid authType value. The valid values are: 'Basic','NTLM','Kerberos'.";
     public static final String EXCEPTION_INVALID_HOSTNAME_VERIFIER = "The %s for %s input is not a valid x509HostnameVerifier value. The valid values are: 'strict','browser_compatible','allow_all'.";
-
-    public static final String EXCEPTION_INVALID_PROXY = "The %s is not a valid port.";
+    public static final String EXCEPTION_INVALID_PROXY = "%s is not a valid proxy.";
 }
