c1c98773287e2979c621521ce95868dfa66e3382
iulia-moldovan <iulia.moldovan@microfocus.com>
2021-07-06 12:30:20 +0300

    changed copyright year

--- cs-active-directory/src/main/java/io/cloudslang/content/active_directory/entities/ResetUserPasswordInput.java
+++ cs-active-directory/src/main/java/io/cloudslang/content/active_directory/entities/ResetUserPasswordInput.java
@@ -1,6 +1,6 @@
 package io.cloudslang.content.active_directory.entities;
 /*
- * (c) Copyright 2020 Micro Focus
+ * (c) Copyright 2021 Micro Focus
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License v2.0 which accompany this distribution.
  *
