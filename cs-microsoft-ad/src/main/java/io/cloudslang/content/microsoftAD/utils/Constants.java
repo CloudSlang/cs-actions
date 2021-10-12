d6f72ccb1004b7c24139822265eeb71970630bb7
iulia-moldovan <iulia.moldovan@microfocus.com>
2021-10-12 10:44:50 +0300

    Added constant

--- cs-microsoft-ad/src/main/java/io/cloudslang/content/microsoftAD/utils/Constants.java
+++ cs-microsoft-ad/src/main/java/io/cloudslang/content/microsoftAD/utils/Constants.java
@@ -34,8 +34,7 @@ public final class Constants {
     public static final String EXCEPTION_INVALID_BOOLEAN = "The %s for %s input is not a valid boolean value.";
     public static final String EXCEPTION_INVALID_NUMBER = "The %s for %s input is not a valid number value.";
     public static final String DEFAULT_JAVA_KEYSTORE = System.getProperty("java.home") + "/lib/security/cacerts";
-    public static final String CHARSET = "charset";
-    public static final String SEMICOLON = ";";
+    public static final String FORWARD_SLASH = "/";
     public static final String EQUALS = "=";
     public static final String CHANGEIT = "changeit";
     public static final String ZERO = "0";
