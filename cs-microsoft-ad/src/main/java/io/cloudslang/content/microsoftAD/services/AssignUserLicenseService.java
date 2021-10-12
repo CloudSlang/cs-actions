fc9d86acdaff6717e2e9755e99c0d72cc01e1a15
ABenta <benta.andinomm@gmail.com>
2021-10-12 14:12:17 +0300

    cleaned stuff

--- cs-microsoft-ad/src/main/java/io/cloudslang/content/microsoftAD/services/AssignUserLicenseService.java
+++ cs-microsoft-ad/src/main/java/io/cloudslang/content/microsoftAD/services/AssignUserLicenseService.java
@@ -14,25 +14,14 @@
  */
 package io.cloudslang.content.microsoftAD.services;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import io.cloudslang.content.microsoftAD.actions.licenseManagement.AssignUserLicense;
 import io.cloudslang.content.microsoftAD.entities.AssignUserLicenseInputs;
 import io.cloudslang.content.microsoftAD.entities.AzureActiveDirectoryCommonInputs;
-import io.cloudslang.content.microsoftAD.utils.Constants;
-import net.minidev.json.JSONArray;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import sun.invoke.empty.Empty;
 
-import java.util.Iterator;
 import java.util.Map;
-
 import static io.cloudslang.content.microsoftAD.services.HttpCommons.httpPost;
 import static io.cloudslang.content.microsoftAD.utils.Constants.*;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 public class AssignUserLicenseService {
 
