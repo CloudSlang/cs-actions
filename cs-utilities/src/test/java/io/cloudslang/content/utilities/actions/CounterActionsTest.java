b165b10deaf9988e8469eb328208f32894d45398
rajesh-kum-kalyankar <rajesh.kum.kalyankar@microfocus.com>
2022-03-14 13:54:46 +0530

    Added the test case for counter

--- cs-utilities/src/test/java/io/cloudslang/content/utilities/actions/CounterActionsTest.java
+++ cs-utilities/src/test/java/io/cloudslang/content/utilities/actions/CounterActionsTest.java
@@ -1,4 +1,18 @@
 /*
+ * (c) Copyright 2021 EntIT Software LLC, a Micro Focus company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * (c) Copyright 2022 Micro Focus, L.P.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License v2.0 which accompany this distribution.
@@ -29,9 +43,6 @@ import static org.junit.Assert.assertEquals;
 @PrepareForTest({CounterActionsTest.class})
 public class CounterActionsTest {
 
-    @Mock
-    private Counter counter;
-
     @Test
     public void runCounter() {
         String from = "1";
@@ -41,7 +52,6 @@ public class CounterActionsTest {
 
         GlobalSessionObject<Map<String, Object>> globalSessionObject = new GlobalSessionObject<>();
         Counter counter = new Counter();
-
         for (int i = Integer.parseInt(from); i < Integer.parseInt(to); i++) {
             Map<String, String> actualResult = counter.execute(from, to, incrementBy, reset, globalSessionObject);
             assertEquals(String.valueOf(i), actualResult.get("resultString"));
