9d8f353b449f628494fb523ca01cef6301414b07
TodereanLenuta <biltiu.lenuta@gmail.com>
2022-03-23 12:09:33 +0200

    Inputs and Outputs files.

---
+++ cs-httpclient-commons/src/main/java/io/cloudslang/content/httpclient/utils/Outputs.java
@@ -0,0 +1,9 @@
+package io.cloudslang.content.httpclient.utils;
+
+import io.cloudslang.content.constants.OutputNames;
+
+public class Outputs extends OutputNames {
+    public static class HTTPClientOutputs {
+        public static final String STATUS_CODE = "statusCode";
+    }
+}
