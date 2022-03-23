bbf48bb811529905444d34726d181a283064f7f3
boicu <boicu@ROBOICU03>
2022-03-23 16:36:21 +0200

    Added execution timeout

---
+++ cs-httpclient-commons/src/main/java/io/cloudslang/content/httpclient/utils/ExecutionTimeout.java
@@ -0,0 +1,37 @@
+package io.cloudslang.content.httpclient.utils;
+
+import java.util.concurrent.*;
+
+public class ExecutionTimeout {
+
+    public static void runWithTimeout(final Runnable runnable, long timeout, TimeUnit timeUnit) throws Exception {
+        runWithTimeout(() -> {
+            runnable.run();
+            return null;
+        }, timeout, timeUnit);
+    }
+
+    public static <T> T runWithTimeout(Callable<T> callable, long timeout, TimeUnit timeUnit) throws Exception {
+        final ExecutorService executor = Executors.newSingleThreadExecutor();
+        final Future<T> future = executor.submit(callable);
+        executor.shutdown(); // This does not cancel the already-scheduled task.
+        try {
+            return future.get(timeout, timeUnit);
+        }
+        catch (TimeoutException e) {
+            future.cancel(true);
+            throw new TimeoutException("The execution timed out after: " + timeout + " " + timeUnit.toString().toLowerCase());
+        }
+        catch (ExecutionException e) {
+            //unwrap the root cause
+            Throwable t = e.getCause();
+            if (t instanceof Error) {
+                throw (Error) t;
+            } else if (t instanceof Exception) {
+                throw (Exception) t;
+            } else {
+                throw new IllegalStateException(t);
+            }
+        }
+    }
+}
