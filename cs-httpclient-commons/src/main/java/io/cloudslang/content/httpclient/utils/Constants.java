0081cee8f8312f548103b6c23d6621addadf6990
boicu <boicu@ROBOICU03>
2022-03-24 17:53:25 +0200

    Added SocketFactory Implementation Changed to the latest library of httpclient5

--- cs-httpclient-commons/src/main/java/io/cloudslang/content/httpclient/utils/Constants.java
+++ cs-httpclient-commons/src/main/java/io/cloudslang/content/httpclient/utils/Constants.java
@@ -16,23 +16,23 @@ package io.cloudslang.content.httpclient.utils;
 
 public class Constants {
 
-    public static final String URL = "url";
-    public static final String TEXT = "text";
-    public static final String CHARACTER_SET = "characterSet";
+    public static final String STRICT = "strict";
+    public static final String ALLOW_ALL = "allow_all";
+    public static final String UTF_8 = "UTF-8";
+    public static final String COMMA = ",";
     public static final String NTLM_AUTH = "NTLM";
     public static final String BASIC_AUTH = "Basic";
     public static final String DEFAULT_JAVA_KEYSTORE = System.getProperty("java.home") + "/lib/security/cacerts";
     public static final String CHANGEIT = "changeit";
     public static final String TLSv12 = "TLSv1.2";
-    public static final String RESPONSE_CHARACTER_SET = "UTF-8";
     public static final String NEW_LINE = "\n";
     public static final String EXCEPTION_INVALID_BOOLEAN = "The %s for %s input is not a valid boolean value.";
     public static final String EXCEPTION_INVALID_VALUE = "%s is not a valid value for executionTimeout. The value must be a positive number.";
     public static final String EXCEPTION_NEGATIVE_VALUE = "%s is not a valid timeout value. The value must be bigger or equal to 0.";
-    public static final String ALLOWED_CIPHERS_VALUE = "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384, TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384," +
-            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, TLS_DHE_RSA_WITH_AES_256_CBC_SHA256, TLS_DHE_RSA_WITH_AES_128_CBC_SHA256," +
-            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384, TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256, TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256," +
-            "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256, TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384, TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384," +
-            "TLS_RSA_WITH_AES_256_GCM_SHA384, TLS_RSA_WITH_AES_256_CBC_SHA256, TLS_RSA_WITH_AES_128_CBC_SHA256";
+    public static final String ALLOWED_CIPHERS_VALUE = "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384," +
+            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_DHE_RSA_WITH_AES_256_CBC_SHA256,TLS_DHE_RSA_WITH_AES_128_CBC_SHA256," +
+            "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256," +
+            "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384," +
+            "TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_256_CBC_SHA256,TLS_RSA_WITH_AES_128_CBC_SHA256";
 
 }
