83f76f98da7f1cd4d8c25d7557738aad011b12e1
rajesh-kum-kalyankar <rajesh.kum.kalyankar@microfocus.com>
2022-05-31 09:26:31 +0530

    added the files

--- cs-google/src/main/java/io/cloudslang/content/google/actions/authentication/ListInstances.java
+++ cs-google/src/main/java/io/cloudslang/content/google/actions/authentication/ListInstances.java
@@ -1,7 +1,7 @@
 package io.cloudslang.content.google.actions.authentication;
 
 import com.google.api.services.compute.Compute;
-import com.google.api.services.compute.model.Instance;
+import com.google.api.services.compute.model.InstanceList;
 import com.hp.oo.sdk.content.annotations.Action;
 import com.hp.oo.sdk.content.annotations.Output;
 import com.hp.oo.sdk.content.annotations.Param;
@@ -12,14 +12,11 @@ import com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType;
 import java.util.HashMap;
 import java.util.Map;
 
-
 import static io.cloudslang.content.google.actions.authentication.Inputs.*;
 import static io.cloudslang.content.utils.OutputUtilities.getFailureResultsMap;
 
-
-
-public class GetInstanceDetails {
-    @Action(name = "Get Instance Details",
+public class ListInstances {
+    @Action(name = "List Instances",
             outputs = {
                     @Output(Outputs.RETURN_CODE),
                     @Output(Outputs.RETURN_RESULT),
@@ -34,7 +31,6 @@ public class GetInstanceDetails {
     public Map<String, String> execute(@Param(value = PROJECT_ID, required = true) String projectId,
                                        @Param(value = ZONE, required = true) String zone,
                                        @Param(value = JSON_TOKEN, required = true) String jsonToken,
-                                       @Param(value = VM_NAME, required = true) String virtualMachineName,
                                        @Param(value = PROXY_HOST) String proxyHost,
                                        @Param(value = PROXY_PORT) String proxyPort,
                                        @Param(value = PROXY_USERNAME) String proxyUsername,
@@ -44,9 +40,9 @@ public class GetInstanceDetails {
 
         try {
             Compute computeService = GoogleComputeImpl.createComputeService(jsonToken, proxyHost, Integer.parseInt(proxyPort));
-            Compute.Instances.Get request = computeService.instances().get(projectId, zone, virtualMachineName);
+            Compute.Instances.List request = computeService.instances().list(projectId, zone);
 
-            Instance response = request.execute();
+            InstanceList response = request.execute();
 
 
             results.put("returnResult", response.toString());
