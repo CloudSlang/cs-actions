b61586009918fc0f39427e5f66ded8ee89dc7ef6
boicu <boicu@ROBOICU03>
2022-06-09 13:37:36 +0300

    refactor

--- cs-oracle-cloud-databases/src/main/java/io/cloudslang/content/database/actions/Main.java
+++ cs-oracle-cloud-databases/src/main/java/io/cloudslang/content/database/actions/Main.java
@@ -12,20 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-/*
- * (c) Copyright 2022 Micro Focus
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Apache License v2.0 which accompany this distribution.
- *
- * The Apache License is available at
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.cloudslang.content.database.actions;
 
 import io.cloudslang.content.database.entities.OracleCloudInputs;
@@ -45,9 +31,9 @@ public class Main {
                 "select * from persons",
                 ";",
                 "",
+                "B33f34t3r",
                 "",
-                "",
-                "",
+                "B33f34t3r",
                 0);
 
         Map<String, String> result = new SQLCommand().execute("(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.eu-frankfurt-1.oraclecloud.com))(connect_data=(service_name=gde34672fa25a61_db202203311236_low.adb.oraclecloud.com))(security=(ssl_server_cert_dn=\"CN=adwc.eucom-central-1.oraclecloud.com, OU=Oracle BMCS FRANKFURT, O=Oracle Corporation, L=Redwood City, ST=California, C=US\")))",
