8462f1fc2a10574e4165da77314afb18a8676983
rajesh-kum-kalyankar <rajesh.kum.kalyankar@microfocus.com>
2022-10-17 11:17:02 +0530

    AWS volume type fix(gp3 is not allowing)

--- cs-amazon/src/main/java/io/cloudslang/content/amazon/entities/aws/VolumeType.java
+++ cs-amazon/src/main/java/io/cloudslang/content/amazon/entities/aws/VolumeType.java
@@ -25,23 +25,12 @@ import static io.cloudslang.content.amazon.entities.constants.Constants.Miscella
  * 6/15/2016.
  */
 public enum VolumeType {
-    STANDARD,
-    IO1,
-    GP2,
-    SC1,
-    ST1;
+    STANDARD;
 
     public static String getValue(String input) {
-        if (isBlank(input)) {
+        if (isBlank(input))
             return NOT_RELEVANT;
-        }
-
-        for (VolumeType member : VolumeType.values()) {
-            if (member.name().equalsIgnoreCase(input)) {
-                return member.name().toLowerCase();
-            }
-        }
-
-        throw new RuntimeException("Unrecognized  volume type value: [" + input + "]. Valid values are: standard, io1, gp2, sc1, st1.");
+        else
+            return input;
     }
 }
