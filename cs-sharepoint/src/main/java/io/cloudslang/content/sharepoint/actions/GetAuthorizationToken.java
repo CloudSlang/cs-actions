47550a9901a28869e77c857f0b21de51c62de266
BoicuAlexandra <36893928+BoicuAlexandra@users.noreply.github.com>
2023-03-02 10:55:52 +0200

    cs-sharepoint get auth token (#1141)
    * Changes on Send Mail Action - ready to read a system property.
    
    * added get auth token operation for cs-sharepoint
    
    * added get auth token operation for cs-sharepoint
    
    ---------
    
    Co-authored-by: TodereanLenuta <biltiu.lenuta@gmail.com>
    Co-authored-by: boicu <boicu@ROBOICU03>

---
+++ cs-sharepoint/src/main/java/io/cloudslang/content/sharepoint/actions/GetAuthorizationToken.java
@@ -0,0 +1,109 @@
+/*
+ * (c) Copyright 2023 Micro Focus, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.cloudslang.content.sharepoint.actions;
+
+import java.util.List;
+import java.util.Map;
+import com.hp.oo.sdk.content.annotations.Action;
+import com.hp.oo.sdk.content.annotations.Output;
+import com.hp.oo.sdk.content.annotations.Param;
+import com.hp.oo.sdk.content.annotations.Response;
+import com.microsoft.aad.msal4j.IAuthenticationResult;
+import io.cloudslang.content.constants.ReturnCodes;
+import io.cloudslang.content.sharepoint.entities.GetAuthorizationTokenInputs;
+import io.cloudslang.content.sharepoint.services.GetAuthorizationTokenImpl;
+import io.cloudslang.content.utils.NumberUtilities;
+import io.cloudslang.content.utils.StringUtilities;
+
+import static com.hp.oo.sdk.content.plugin.ActionMetadata.MatchType.COMPARE_EQUAL;
+import static com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType.ERROR;
+import static com.hp.oo.sdk.content.plugin.ActionMetadata.ResponseType.RESOLVED;
+import static io.cloudslang.content.constants.OutputNames.*;
+import static io.cloudslang.content.constants.ResponseNames.*;
+import static io.cloudslang.content.sharepoint.utils.Constants.*;
+import static io.cloudslang.content.sharepoint.utils.Descriptions.Common.*;
+import static io.cloudslang.content.sharepoint.utils.Descriptions.GetAuthorizationToken.*;
+import static io.cloudslang.content.sharepoint.utils.Descriptions.GetAuthorizationToken.RETURN_CODE_DESC;
+import static io.cloudslang.content.sharepoint.utils.Inputs.AuthorizationInputs.*;
+import static io.cloudslang.content.sharepoint.utils.Inputs.CommonInputs.*;
+import static io.cloudslang.content.sharepoint.utils.InputsValidation.verifyAuthorizationInputs;
+import static io.cloudslang.content.sharepoint.utils.Outputs.*;
+import static io.cloudslang.content.sharepoint.utils.Outputs.EXCEPTION;
+import static io.cloudslang.content.utils.OutputUtilities.getFailureResultsMap;
+import static io.cloudslang.content.utils.OutputUtilities.getSuccessResultsMap;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
+
+public class GetAuthorizationToken {
+    @Action(name = "Get the authorization token for Office 365 Sharepoint",
+            outputs = {
+                    @Output(value = RETURN_RESULT, description = RETURN_RESULT_DESC),
+                    @Output(value = RETURN_CODE, description = RETURN_CODE_DESC),
+                    @Output(value = AUTH_TOKEN, description = AUTH_TOKEN_DESC),
+                    @Output(value = EXCEPTION, description = EXCEPTION_DESC)
+            },
+            responses = {
+                    @Response(text = SUCCESS, field = RETURN_CODE, value = ReturnCodes.SUCCESS, matchType = COMPARE_EQUAL, responseType = RESOLVED, description = SUCCESS_DESC),
+                    @Response(text = FAILURE, field = RETURN_CODE, value = ReturnCodes.FAILURE, matchType = COMPARE_EQUAL, responseType = ERROR, description = FAILURE_DESC)
+            })
+    public Map<String, String> execute(@Param(value = LOGIN_TYPE, description = LOGIN_TYPE_DESC) String loginType,
+                                       @Param(value = CLIENT_ID, required = true, description = CLIENT_ID_DESC) String clientId,
+                                       @Param(value = CLIENT_SECRET, encrypted = true, description = CLIENT_SECRET_DESC) String clientSecret,
+                                       @Param(value = USERNAME, description = USERNAME_DESC) String username,
+                                       @Param(value = PASSWORD, encrypted = true, description = PASSWORD_DESC) String password,
+                                       @Param(value = LOGIN_AUTHORITY, required = true, description = LOGIN_AUTHORITY_DESC) String loginAuthority,
+                                       @Param(value = SCOPE, description = SCOPE_DESC) String scope,
+                                       @Param(value = PROXY_HOST, description = PROXY_HOST_DESC) String proxyHost,
+                                       @Param(value = PROXY_PORT, description = PROXY_PORT_DESC) String proxyPort,
+                                       @Param(value = PROXY_USERNAME, description = PROXY_USERNAME_DESC) String proxyUsername,
+                                       @Param(value = PROXY_PASSWORD, encrypted = true, description = PROXY_PASSWORD_DESC) String proxyPassword) {
+        loginType = defaultIfEmpty(loginType, DEFAULT_LOGIN_TYPE);
+        clientId = defaultIfEmpty(clientId, EMPTY);
+        clientSecret = defaultIfEmpty(clientSecret, EMPTY);
+        username = defaultIfEmpty(username, EMPTY);
+        password = defaultIfEmpty(password, EMPTY);
+        scope = defaultIfEmpty(scope, DEFAULT_SCOPE);
+        proxyHost = defaultIfEmpty(proxyHost, EMPTY);
+        proxyPort = defaultIfEmpty(proxyPort, DEFAULT_PROXY_PORT);
+        proxyUsername = defaultIfEmpty(proxyUsername, EMPTY);
+        proxyPassword = defaultIfEmpty(proxyPassword, EMPTY);
+        final List<String> exceptionMessages = verifyAuthorizationInputs(loginType, clientId, clientSecret, username, password, proxyPort);
+        if (!exceptionMessages.isEmpty()) {
+            return getFailureResultsMap(StringUtilities.join(exceptionMessages, NEW_LINE));
+        }
+
+        try {
+            final IAuthenticationResult result = GetAuthorizationTokenImpl.getToken(GetAuthorizationTokenInputs.builder()
+                    .loginType(loginType)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .username(username)
+                    .password(password)
+                    .authority(loginAuthority)
+                    .scope(scope)
+                    .proxyHost(proxyHost)
+                    .proxyPort(NumberUtilities.toInteger(proxyPort))
+                    .proxyUsername(proxyUsername)
+                    .proxyPassword(proxyPassword)
+                    .build());
+
+            final Map<String, String> successResultsMap = getSuccessResultsMap(result.accessToken());
+            successResultsMap.put(AUTH_TOKEN, result.accessToken());
+            return successResultsMap;
+        } catch (Exception exception) {
+            return getFailureResultsMap(exception);
+        }
+    }
+}
---
+++ cs-sharepoint/src/main/java/io/cloudslang/content/sharepoint/utils/Descriptions.java
@@ -0,0 +1,86 @@
+/*
+ * (c) Copyright 2023 Micro Focus, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License v2.0 which accompany this distribution.
+ *
+ * The Apache License is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.cloudslang.content.sharepoint.utils;
+
+public class Descriptions {
+    public static class Common {
+        public static final String PROXY_HOST_DESC = "Proxy server used to access the Office 365 service.";
+        public static final String PROXY_PORT_DESC = "Proxy server port used to access the Office 365 service." +
+                "Default: '8080'";
+        public static final String PROXY_USERNAME_DESC = "Proxy server user name.";
+        public static final String PROXY_PASSWORD_DESC = "Proxy server password associated with the proxy_username input value.";
+        public static final String TRUST_ALL_ROOTS_DESC = "Specifies whether to enable weak security over SSL/TSL. " +
+                "A certificate is trusted even if no trusted certification authority issued it.";
+        public static final String X509_DESC = "Specifies the way the server hostname must match a domain name in " +
+                "the subject's Common Name (CN) or subjectAltName field of the X.509 certificate. Set this to " +
+                "\"allow_all\" to skip any checking. For the value \"browser_compatible\" the hostname verifier " +
+                "works the same way as Curl and Firefox. The hostname must match either the first CN, or any of " +
+                "the subject-alts. A wildcard can occur in the CN, and in any of the subject-alts. The only " +
+                "difference between \"browser_compatible\" and \"strict\" is that a wildcard (such as \"*.foo.com\") " +
+                "with \"browser_compatible\" matches all subdomains, including \"a.b.foo.com\".";
+        public static final String TRUST_KEYSTORE_DESC = "The pathname of the Java TrustStore file. This contains " +
+                "certificates from other parties that you expect to communicate with, or from Certificate Authorities" +
+                " that you trust to identify other parties.  If the protocol (specified by the 'url') is not 'https' " +
+                "or if trustAllRoots is 'true' this input is ignored. Format: Java KeyStore (JKS)";
+        public static final String TRUST_PASSWORD_DESC = "The password associated with the TrustStore file. If " +
+                "trustAllRoots is false and trustKeystore is empty, trustPassword default will be supplied.";
+        public static final String CONN_MAX_TOTAL_DESC = "The maximum limit of connections in total.";
+        public static final String CONN_MAX_ROUTE_DESC = "The maximum limit of connections on a per route basis.";
+        public static final String KEEP_ALIVE_DESC = "Specifies whether to create a shared connection that will be " +
+                "used in subsequent calls. If keepAlive is false, the already open connection will be used and after" +
+                " execution it will close it.";
+        public static final String SOCKET_TIMEOUT_DESC = "The timeout for waiting for data (a maximum period " +
+                "inactivity between two consecutive data packets), in seconds. A socketTimeout value of '0' " +
+                "represents an infinite timeout.";
+        public static final String CONNECT_TIMEOUT_DESC = "The time to wait for a connection to be established, " +
+                "in seconds. A timeout value of '0' represents an infinite timeout.";
+        public static final String RESPONSE_CHARACTER_SET_DESC = "The character encoding to be used for the HTTP response. " +
+                "If responseCharacterSet is empty, the charset from the 'Content-Type' HTTP response header will be used. " +
+                "If responseCharacterSet is empty and the charset from the HTTP response Content-Type header is empty, the " +
+                "default value will be used. You should not use this for method=HEAD or OPTIONS.\n" +
+                "Default value: UTF-8";
+
+        public static final String RETURN_CODE_DESC = "0 if success, -1 otherwise.";
+    }
+
+    public static class GetAuthorizationToken {
+        public static final String LOGIN_TYPE_DESC = "Login method according to application type\n" +
+                "Valid values: 'API', 'Native'\n" +
+                "Default: 'API'";
+        public static final String CLIENT_ID_DESC = "Service Client ID";
+        public static final String CLIENT_SECRET_DESC = "Service Client Secret";
+        public static final String USERNAME_DESC = "Office 365 username";
+        public static final String PASSWORD_DESC = "Office 365 password";
+        public static final String LOGIN_AUTHORITY_DESC = "The authority URL. Usually, the format for this input is:\n" +
+                "'https://login.windows.net/TENANT_NAME/oauth2/token' where TENANT_NAME is your application\n" +
+                "tenant.";
+        public static final String RESOURCES_DESC = "The resource URL\n" +
+                "Default: 'https://graph.microsoft.com'";
+        public static final String SCOPE_DESC = "The scope URL. The scope consists of a series of resource permissions separated " +
+                "by a comma (,), i.e.: 'https://graph.microsoft.com/User.Read, https://graph.microsoft.com/Sites.Read'. " +
+                "The 'https://graph.microsoft.com/.default' scope means that the user consent to all of the configured permissions " +
+                "present on the specific Azure AD Application. For the 'API' loginType, '/.default' scope should be used. \n" +
+                "Default: 'https://graph.microsoft.com/.default'";
+
+        public static final String RETURN_RESULT_DESC = "The authorization token for Office 365.";
+        public static final String RETURN_CODE_DESC = "0 if success, -1 otherwise.";
+        public static final String AUTH_TOKEN_DESC = "The authentication token.";
+        public static final String AUTH_TOKEN_TYPE_DESC = "The authentication token type.";
+        public static final String EXCEPTION_DESC = "An error message in case there was an error while generating the token.";
+
+        public static final String SUCCESS_DESC = "Token generated successfully.";
+        public static final String FAILURE_DESC = "There was an error while trying to retrieve token.";
+    }
+}
