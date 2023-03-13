10defcdc21f21193670baf2962323c8dab352506
boicu <boicu@ROBOICU03>
2023-03-13 14:20:19 +0200

    refactor

--- cs-sharepoint/src/main/java/io/cloudslang/content/sharepoint/actions/sites/GetSiteIdByName.java
+++ cs-sharepoint/src/main/java/io/cloudslang/content/sharepoint/actions/sites/GetSiteIdByName.java
@@ -50,7 +50,7 @@ public class GetSiteIdByName {
                             @Response(text = FAILURE, field = RETURN_CODE, value = ReturnCodes.FAILURE, matchType = COMPARE_EQUAL, responseType = ERROR, description = FAILURE_DESC)
                     })
 
-    public Map<String, String> execute(@Param(value = AUTH_TOKEN, required = true, description = AUTH_TOKEN_DESC) String authToken,
+    public Map<String, String> execute(@Param(value = AUTH_TOKEN, required = true, encrypted = true, description = AUTH_TOKEN_DESC) String authToken,
                                        @Param(value = SITE_NAME, required = true, description = SITE_NAME_DESC) String siteName,
 
                                        @Param(value = PROXY_HOST, description = PROXY_HOST_DESC) String proxyHost,
