d570fb0e6b1ff88b4627880a0f277f92b2b18364
chavva-susmitha <csusmitha@opentext.com>
2026-06-10 09:49:12 +0530

    changes for scp issue

--- cs-rft/src/main/java/io/cloudslang/content/rft/services/SCPCopier.java
+++ cs-rft/src/main/java/io/cloudslang/content/rft/services/SCPCopier.java
@@ -20,10 +20,13 @@ import com.jcraft.jsch.*;
 import io.cloudslang.content.rft.entities.*;
 import io.cloudslang.content.rft.utils.Constants;
 import io.cloudslang.content.rft.utils.StringUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.Provider;
+import java.security.Security;
 import java.util.Locale;
 
 import static io.cloudslang.content.rft.utils.Constants.BACKSLASH;
@@ -39,6 +42,10 @@ public class SCPCopier {
     private static final String KNOWN_HOSTS_ALLOW = "allow";
     private static final String KNOWN_HOSTS_STRICT = "strict";
     private static final String KNOWN_HOSTS_ADD = "add";
+    private static final String ADDITIONAL_CIPHERS = ",aes128-ctr,aes128-cbc,3des-ctr,3des-cbc,blowfish-cbc,aes192-ctr,aes192-cbc,aes256-ctr,aes256-cbc";
+    private static final String KEX_ALGORITHMS = ",curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group18-sha512,diffie-hellman-group16-sha512,diffie-hellman-group14-sha256,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha1,diffie-hellman-group1-sha1";
+    private static final String ADDITIONAL_HOST_KEY_ALGORITHMS = ",ssh-ed25519,rsa-sha2-512,rsa-sha2-256,ssh-rsa,ssh-dss";
+    private static final String ADDITIONAL_PUBKEY_ALGORITHMS = ",ssh-ed25519,rsa-sha2-512,rsa-sha2-256,ssh-rsa,ssh-dss";
 
     private Session session;
     private RemoteSecureCopyInputs remoteSecureCopyInputs;
@@ -65,18 +72,34 @@ public class SCPCopier {
         return copyFromRemoteToLocal(remoteSecureCopyInputs.getSrcPath(), remoteSecureCopyInputs.getDestPath());
     }
 
+    private boolean addSecurityProvider() {
+        boolean providerAdded = false;
+        Provider provider = Security.getProvider("BC");
+        if (provider == null) {
+            providerAdded = true;
+            Security.insertProviderAt(new BouncyCastleProvider(), 2);
+        }
+        return providerAdded;
+    }
+
+    private void removeSecurityProvider() {
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+    }
+
     protected boolean copyFromLocalToRemote(String srcPath, String destPath){
         FileInputStream fileInputStream = null;
+        boolean providerAdded = addSecurityProvider();
 
         try {
 
             JSch jsch = new JSch();
+            configureAlgorithms();
             session = jsch.getSession(remoteSecureCopyInputs.getDestUsername(), remoteSecureCopyInputs.getDestHost(), StringUtils.toInt(remoteSecureCopyInputs.getDestPort(), Constants.DEFAULT_PORT));
+            session.setConfig("PreferredAuthentications", "publickey,password,keyboard-interactive");
 
             String proxyHost = remoteSecureCopyInputs.getProxyHost();
             if (!StringUtils.isEmpty(proxyHost)) session.setProxy(new ProxyHTTP(proxyHost, StringUtils.toInt(remoteSecureCopyInputs.getProxyPort(), Constants.DEFAULT_PROXY_PORT)));
 
-            configureModernAlgorithms(session);
             establishKnownHostsConfiguration(ConnectionUtils.resolveKnownHosts(remoteSecureCopyInputs.getKnownHostsPolicy(), remoteSecureCopyInputs.getKnownHostsPath()), jsch, session);
             establishPrivateKeyFile(ConnectionUtils.getKeyFile(remoteSecureCopyInputs.getDestPrivateKeyFile(), remoteSecureCopyInputs.getDestPassword()), jsch, session, false);
 
@@ -153,21 +176,26 @@ public class SCPCopier {
                     fileInputStream.close();
                 }
             }catch (IOException ioe) {}
+            if (providerAdded) {
+                removeSecurityProvider();
+            }
         }
     }
 
     protected boolean copyFromRemoteToLocal(String srcPath, String destPath){
         FileOutputStream fileOutputStream = null;
+        boolean providerAdded = addSecurityProvider();
 
         try {
 
             JSch jsch = new JSch();
+            configureAlgorithms();
             session = jsch.getSession(remoteSecureCopyInputs.getSrcUsername(), remoteSecureCopyInputs.getSrcHost(), StringUtils.toInt(remoteSecureCopyInputs.getSrcPort(), Constants.DEFAULT_PORT));
+            session.setConfig("PreferredAuthentications", "publickey,password,keyboard-interactive");
 
             String proxyHost = remoteSecureCopyInputs.getProxyHost();
             if (!StringUtils.isEmpty(proxyHost)) session.setProxy(new ProxyHTTP(proxyHost, StringUtils.toInt(remoteSecureCopyInputs.getProxyPort(), Constants.DEFAULT_PROXY_PORT)));
 
-            configureModernAlgorithms(session);
             establishKnownHostsConfiguration(ConnectionUtils.resolveKnownHosts(remoteSecureCopyInputs.getKnownHostsPolicy(), remoteSecureCopyInputs.getKnownHostsPath()), jsch, session);
             establishPrivateKeyFile(ConnectionUtils.getKeyFile(remoteSecureCopyInputs.getSrcPrivateKeyFile(), remoteSecureCopyInputs.getSrcPassword()), jsch, session, true);
 
@@ -262,6 +290,9 @@ public class SCPCopier {
             catch (Exception ee){
 
             }
+            if (providerAdded) {
+                removeSecurityProvider();
+            }
         }
     }
 
@@ -303,27 +334,34 @@ public class SCPCopier {
         else {
             String keyFilePath = keyFile.getKeyFilePath();
             String passPhrase = keyFile.getPassPhrase();
-            if (passPhrase != null){
-                jsch.addIdentity(keyFilePath, passPhrase);
-            } else {
-                jsch.addIdentity(keyFilePath);
-            }
+            byte[] passphraseBytes = (passPhrase != null) ? passPhrase.getBytes(java.nio.charset.StandardCharsets.UTF_8) : null;
+            jsch.addIdentity(keyFilePath, passphraseBytes);
         }
     }
 
-    private void configureModernAlgorithms(Session session) {
-        session.setConfig("kex",
-                "curve25519-sha256,curve25519-sha256@libssh.org," +
-                "ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521," +
-                "diffie-hellman-group18-sha512,diffie-hellman-group16-sha512," +
-                "diffie-hellman-group14-sha256,diffie-hellman-group14-sha1");
-        session.setConfig("server_host_key",
-                "ssh-ed25519,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521," +
-                "rsa-sha2-512,rsa-sha2-256,ssh-rsa");
-        session.setConfig("PubkeyAcceptedAlgorithms",
-                "ssh-ed25519,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521," +
-                "rsa-sha2-512,rsa-sha2-256,ssh-rsa");
+    private void configureAlgorithms() {
+        String ciphersS2C = JSch.getConfig("cipher.s2c");
+        if (ciphersS2C == null || !ciphersS2C.contains("aes128-ctr")) {
+            JSch.setConfig("cipher.s2c", (ciphersS2C != null ? ciphersS2C : "") + ADDITIONAL_CIPHERS);
+        }
+        String ciphersC2S = JSch.getConfig("cipher.c2s");
+        if (ciphersC2S == null || !ciphersC2S.contains("aes128-ctr")) {
+            JSch.setConfig("cipher.c2s", (ciphersC2S != null ? ciphersC2S : "") + ADDITIONAL_CIPHERS);
+        }
+        String serverHostKey = JSch.getConfig("server_host_key");
+        if (serverHostKey == null || !serverHostKey.contains("rsa-sha2-256")) {
+            JSch.setConfig("server_host_key", (serverHostKey != null ? serverHostKey : "") + ADDITIONAL_HOST_KEY_ALGORITHMS);
+        }
+        String pubkeyAlgorithms = JSch.getConfig("PubkeyAcceptedAlgorithms");
+        if (pubkeyAlgorithms == null || !pubkeyAlgorithms.contains("rsa-sha2-256")) {
+            JSch.setConfig("PubkeyAcceptedAlgorithms", (pubkeyAlgorithms != null ? pubkeyAlgorithms : "") + ADDITIONAL_PUBKEY_ALGORITHMS);
+        }
+        String kex = JSch.getConfig("kex");
+        if (kex == null || !kex.contains("curve25519-sha256")) {
+            JSch.setConfig("kex", (kex != null ? kex : "") + KEX_ALGORITHMS);
+        }
     }
+
     private int checkAck(InputStream in) throws IOException {
         int b = in.read();
         // b may be 0 for success,
