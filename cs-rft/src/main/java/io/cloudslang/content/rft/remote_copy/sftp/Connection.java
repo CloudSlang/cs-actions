b7cf3bc35135531818b591812b4fdd09da12dc99
ykona <ykona@opentext.com>
2026-06-11 12:04:20 +0530

    revert revert "changes for scp issue"
    This reverts commit d570fb0e6b1ff88b4627880a0f277f92b2b18364.

--- cs-rft/src/main/java/io/cloudslang/content/rft/remote_copy/sftp/Connection.java
+++ cs-rft/src/main/java/io/cloudslang/content/rft/remote_copy/sftp/Connection.java
@@ -21,11 +21,6 @@ import com.jcraft.jsch.*;
 
 public class Connection {
 
-    private static final String ADDITIONAL_CIPHERS = ",aes128-ctr,aes128-cbc,3des-ctr,3des-cbc,blowfish-cbc,aes192-ctr,aes192-cbc,aes256-ctr,aes256-cbc";
-    private static final String KEX_ALGORITHMS = ",curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group18-sha512,diffie-hellman-group16-sha512,diffie-hellman-group14-sha256,diffie-hellman-group-exchange-sha256,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha1,diffie-hellman-group1-sha1";
-    private static final String ADDITIONAL_HOST_KEY_ALGORITHMS = ",ssh-ed25519,rsa-sha2-512,rsa-sha2-256,ssh-rsa,ssh-dss";
-    private static final String ADDITIONAL_PUBKEY_ALGORITHMS = ",ssh-ed25519,rsa-sha2-512,rsa-sha2-256,ssh-rsa,ssh-dss";
-
    // private static Logger logger = LoggerFactory.getLogger(Connection.class);
     private String username;
     private String password;
@@ -71,7 +66,6 @@ public class Connection {
 
         if (session == null || !session.isConnected()) {
             JSch jsch = new JSch();
-            configureAlgorithms();
 
             // username and password will be given via UserInfo interface.
             MyUserInfo uInfo = new MyUserInfo();
@@ -109,29 +103,6 @@ public class Connection {
 
     public String getHost() {
         return host;
-    }
-
-    private void configureAlgorithms() {
-        String ciphersS2C = JSch.getConfig("cipher.s2c");
-        if (ciphersS2C == null || !ciphersS2C.contains("aes128-ctr")) {
-            JSch.setConfig("cipher.s2c", (ciphersS2C != null ? ciphersS2C : "") + ADDITIONAL_CIPHERS);
-        }
-        String ciphersC2S = JSch.getConfig("cipher.c2s");
-        if (ciphersC2S == null || !ciphersC2S.contains("aes128-ctr")) {
-            JSch.setConfig("cipher.c2s", (ciphersC2S != null ? ciphersC2S : "") + ADDITIONAL_CIPHERS);
-        }
-        String serverHostKey = JSch.getConfig("server_host_key");
-        if (serverHostKey == null || !serverHostKey.contains("rsa-sha2-256")) {
-            JSch.setConfig("server_host_key", (serverHostKey != null ? serverHostKey : "") + ADDITIONAL_HOST_KEY_ALGORITHMS);
-        }
-        String pubkeyAlgorithms = JSch.getConfig("PubkeyAcceptedAlgorithms");
-        if (pubkeyAlgorithms == null || !pubkeyAlgorithms.contains("rsa-sha2-256")) {
-            JSch.setConfig("PubkeyAcceptedAlgorithms", (pubkeyAlgorithms != null ? pubkeyAlgorithms : "") + ADDITIONAL_PUBKEY_ALGORITHMS);
-        }
-        String kex = JSch.getConfig("kex");
-        if (kex == null || !kex.contains("curve25519-sha256")) {
-            JSch.setConfig("kex", (kex != null ? kex : "") + KEX_ALGORITHMS);
-        }
     }
 
     public void setHost(String host) {
